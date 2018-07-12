4d2672bca9608e67a8d41625b5acda77e599e631
haoran.li <haoran.li@msxf.com>
2018-07-12 09:45:38 +0800

    new file

---
+++ GeoInfo记录.js
@@ -0,0 +1,186 @@
+/**
+ * 地理信息相关
+ * H5
+ * 百度Api
+ */
+
+=> H5 
+	oBtn.onclick = function () {
+		navigator.geolocation.getCurrentPosition(pos => {
+			oTxt.value = pos.coords.longitude;  // 经度
+			/*
+			 coords.latitude 纬度
+			 coords.accuracy 精确度
+			 coords.altitude 海拔
+			 coords.altitudeAccuracy 海拔精确度
+			 coords.heading 行进方法
+			 coords.speed 地面速度
+			 new Date(pos.timestamp) 时间戳
+			*/
+		})
+	}
+
+	oBtn.onclick = function () {
+		// 单次定位请求
+		navigator.geolocation.getCurrentPosition();
+		// 多次定位请求
+		navigator.geolocation.watchPosition();
+	}
+
+=> BaiduMap
+	> 入门指南
+		1.创建地图实例
+			/*位于BMap命名空间下的Map类表示地图，通过new操作符可以创建一个地图实例。其参数可以是元素id也可以是元素对象。*/
+			let map = new BMap.Map('container')
+		2.设置中心点坐标
+			let point = new BMap.Point(longitude, latitude);
+		3.地图初始化，同时设置地图展示级别
+			在创建地图实例后，我们需要对其进行初始化，BMap.Map.centerAndZoom()方法要求设置中心点坐标和地图级别。 地图必须经过初始化才可以执行其他操作。
+			map.centerAndZoom(point, 15);  	
+		4.开启鼠标滚轮缩放	
+			map.enableScrollWheelZoom(true);  // 开启鼠标滚轮缩放
+	> 添加控件
+		抽象基类       Control                  所有的控件继承此类的方法、属性。通过此类你可以现实自定义控件  
+		平移缩放控件   NavigationControl        PC端默认位于地图左上角，它包含控制地图的平衡和缩放功能。移动端提供缩放控件，默认位于地图右下方。
+		缩略地图       OverviewMapControl       默认位于地图右下方，是一个可折叠的缩略地图
+		比例尺        ScaleControl              默认位于地图左下方，显示地图的比例关系
+		地图类型      MapTypeControl            默认位于地图右上方
+		版权          CopyrightControl          默认位于地图左下方
+		定位          GeolocationControl        针对移动端开发，默认位于地图左下方
+
+	map.addControl(new BMap.NavigationControl()); 
+
+	> 地图的覆盖物
+		可以使用map.addOverlay方法向地图添加覆盖物，使用map.removeOverlay方法移除覆盖物，注意此方法不适用于InfoWindow。
+		抽象基类     Overlay       所有的覆盖物均继承此类的方法
+		点  	        Marker	      表示地图上的点，可自定义标注的图标
+		信息窗口	    InfoWindow  	信息窗口也是一种特殊的覆盖物，它可以展示更为丰富的文字和多媒体信息。注意：同一时刻只能有一个信息窗口在地图上打开
+
+		1.向地图添加标注
+			如下示例，向地图中心点添加了一个标注，并使用默认的标注样式：
+			var point = new BMap.Point(116.404, 39.915);       
+			var marker = new BMap.Marker(point);        // 创建标注    
+			map.addOverlay(marker);                     // 将标注添加到地图中 	
+		2.监听标事件	
+			marker.addEventListener('click', () => {
+				alert('你点击了标');
+			});
+
+		3.可拖拽的标注
+			marker的enableDragging和disableDragging方法可用来开启和关闭标注的拖拽功能。默认情况下标注不支持拖拽，您需要调用marker.enableDragging()方法来开启拖拽功能。在标注开启拖拽功能后，您可以监听标注的dragend事件来捕获拖拽后标注的最新位置。
+			marker.enableDragging();    
+			marker.addEventListener("dragend", function(e){    
+			    alert("当前位置：" + e.point.lng + ", " + e.point.lat);    
+			});  	
+
+	> 自定义标注	
+		1.定义构造函数并继承Overlay
+			首先您需要定义自定义覆盖物的构造函数，通过构造函数参数可以传递一些自由的变量。设置自定义覆盖物对象的prototype属性为Overlay的实例，以便继承覆盖物基类。
+			如下示例，我们定义一个名为SquareOverlay的构造函数，它包含中心点和边长两个参数，用来在地图上创建一个方形覆盖物。
+			// 定义自定义覆盖物的构造函数  
+			function SquareOverlay(center, length, color) {
+			    this._center = center;
+			    this._length = length;
+			    this._color = color;
+			}
+			// 继承API的BMap.Overlay
+			SquareOverlay.prototype = new BMap.Overlay();
+		2.初始化自定义覆盖物
+			实现initialize方法，当调用map.addOverlay方法时，API会调用此方法。
+			当调用map.addOverlay方法添加自定义覆盖物时，API会调用该对象的initialize方法用来初始化覆盖物，在初始化过程中需要创建覆盖物所需要的DOM元素，并添加到地图相应的容器中。这里我们选择添加在容器markerPane上。
+			// 实现初始化方法  
+			SquareOverlay.prototype.initialize = function(map) {
+		    // 保存map对象实例
+		    this._map = map;
+		    // 创建div元素，作为自定义覆盖物的容器
+		    let div = document.createElement("div");
+		    div.style.position = "absolute";
+		    // 可以根据参数设置元素外观
+		    div.style.width = this._length + "px";
+		    div.style.height = this._length + "px";
+		    div.style.background = this._color;
+		    // 将div添加到覆盖物容器中
+		    map.getPanes().markerPane.appendChild(div);
+		    // 保存div实例
+		    this._div = div;
+		    // 需要将div元素作为方法的返回值，当调用该覆盖物的show、
+		    // hide方法，或者对覆盖物进行移除时，API都将操作此元素。
+		    return div;
+			}
+			地图提供了若干容器供覆盖物展示，通过map.getPanes方法可以得到这些容器元素，它们包括：
+				floatPane
+				markerMouseTarget
+				floatShadow
+				labelPane
+				markerPane
+				mapPane
+			这些对象代表了不同的覆盖物容器元素，它们之间存在着覆盖关系，最上一层为floatPane，用于显示信息窗口内容，下面依次为标注点击区域层、信息窗口阴影层、文本标注层、标注层和矢量图形层。
+			我们自定义的方形覆盖物可以添加到任意图层上，如上示例，我们选择添加到markerPane上，作为其一个子结点。
+		3.绘制覆盖物
+			实现draw方法。
+			到目前为止，我们仅仅把覆盖物添加到了地图上，但是并没有将它放置在正确的位置上。
+			您需要在draw方法中设置覆盖物的位置，每当地图状态发生变化（比如：位置移动、级别变化）时，API都会调用覆盖物的draw方法，用于重新计算覆盖物的位置。
+			通过map.pointToOverlayPixel方法可以将地理坐标转换到覆盖物的所需要的像素坐标。
+			// 实现绘制方法   
+			SquareOverlay.prototype.draw = function(){    
+				// 根据地理坐标转换为像素坐标，并设置给容器    
+		    let position = this._map.pointToOverlayPixel(this._center);    
+		    this._div.style.left = position.x - this._length / 2 + "px";    
+		    this._div.style.top = position.y - this._length / 2 + "px";    
+			}
+		4.移除覆盖物
+			当调用map.removeOverlay或者map.clearOverlays方法时，API会自动将initialize方法返回的DOM元素进行移除。
+		5.显示和隐藏覆盖物
+			自定义覆盖物会自动继承Overlay的show和hide方法，方法会修改由initialize方法返回的DOM元素的style.display属性。
+			如果自定义覆盖物元素较为复杂，您也可以自己实现show和hide方法。
+			// 实现显示方法    
+			SquareOverlay.prototype.show = function(){    
+			    if (this._div){    
+			        this._div.style.display = "";    
+			    }    
+			}      
+			// 实现隐藏方法  
+			SquareOverlay.prototype.hide = function(){    
+			    if (this._div){    
+			        this._div.style.display = "none";    
+			    }    
+			}
+			自定义其他方法 通过构造函数的prototype属性，您可以添加任何自定义的方法，比如下面这个方法每调用一次就能改变覆盖物的显示状态：
+			// 添加自定义方法   
+			SquareOverlay.prototype.toggle = function(){    
+			    if (this._div){    
+			        if (this._div.style.display == ""){    
+			            this.hide();    
+			        }    
+			        else {    
+			            this.show();    
+			        }    
+			    }    
+			}
+		6.添加覆盖物
+			您现在已经完成了一个完整的自定义覆盖物的编写，可以添加到地图上了。
+			// 初始化地图  
+			let map = new BMap.Map("container");    
+			let point = new BMap.Point(116.404, 39.915);    
+			map.centerAndZoom(point, 15);    
+			// 添加自定义覆盖物   
+			let mySquare = new SquareOverlay(map.getCenter(), 100, "red");    
+			map.addOverlay(mySquare);
+
+	> 信息窗口	
+		添加信息窗口
+			信息窗口在地图的上方浮动显示HTML内容
+			信息窗口可直接在地图上的任意位置打开，也可以在标注对象上打开（此时信息窗口的坐标与标注的坐标一致）。
+			您可以使用InfoWindow来创建一个信息窗实例，
+			注意同一时刻地图上只能有一个信息窗口处于打开状态。
+
+			let opts = {
+				width: 250,     // 信息窗口宽度
+				height: 100,    // 信息窗口高度
+				title: 'Hello'  // 信息窗口标题
+			}
+
+			let infoWindow = new BMap.InfoWindow("window", opts); // 创建信息窗口对象
+			map.openInfoWindow(infoWindow, map.getCenter());      // 打开信息窗口
+
+			
---
+++ Vue-Element-UI记录.vue
@@ -0,0 +1,58 @@
+=> Layout 布局
+	通过基础的24分栏，迅速简便地创建布局；
+	<el-row></el-row>
+	<el-col :span="24"></el-col>
+
+	Row组件提供gutter属性来指定每一栏之间的间隔
+	<el-row :gutter="6"></el-row>
+	
+	>支持偏移指定的栏数
+	通过制定col组件的offset属性可以指定分栏领衔偏移的栏数;
+	<el-col :span="6" :offset="6"></el-col>
+
+	>对齐方式
+	通过flex布局来对分进行灵活的对齐
+	将type属性赋值为'flex'，可以启用flex布局，并可通过justify属性来指定start,center,end,space-between,space-around其中的值来定义子元素的排版方式；
+	<el-row type="flex" class="row-bg" justify="center">
+  	<el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+  	<el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+  	<el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+	</el-row>
+
+	>响应式布局
+	参照了Bootstrap的响应式布局，预设了五个响应尺寸：xs,sm,md,lg和xl。
+	<el-row :gutter="10">
+	  <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"><div class="grid-content bg-purple"></div></el-col>
+	  <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"><div class="grid-content bg-purple-light"></div></el-col>
+	  <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11"><div class="grid-content bg-purple"></div></el-col>
+	  <el-col :xs="8" :sm="6" :md="4" :lg="3" :xl="1"><div class="grid-content bg-purple-light"></div></el-col>
+	</el-row>
+
+	$$ Row Attributes
+		gutter  栅格间隔                             number 
+		type    布局模式，可选flex，现代浏览器下有效   string  
+		justify flex布局下的水平排列方式              string  start/end/center/space-around/space-between
+		algin   flex布局下的垂直排列方式              string  top/middle/bottom
+		tag     自定义元素标签                        string
+
+	$$ Col Attributes	
+		span    栅格占据的列数                        number
+		offset  栅格左侧的间隔格数                     number
+		push    栅格向右移动格数                       number
+		pull    栅格向左移动格数                       number
+		xs      <768px 响应式格数或者栅格属性对象       number/object(例如：{span:4, offset:4})
+		sm      ≥768px 响应式栅格数或者栅格属性对象     number/object (例如： {span: 4, offset: 4})
+		md      ≥992px 响应式栅格数或者栅格属性对象     number/object (例如： {span: 4, offset: 4})
+		lg	    ≥1200px 响应式栅格数或者栅格属性对象	   number/object (例如： {span: 4, offset: 4})
+		xl	    ≥1920px 响应式栅格数或者栅格属性对象	   number/object (例如： {span: 4, offset: 4})
+		tag	    自定义元素标签	                       string
+
+=> Container 布局容器
+	用于布局的窗口组件，方便快速搭建面面的基本结构：
+	<el-container></el-container>: 外层容器。当子元素包含<el-header>或<el-footer>时，全部子元素垂直上下排列，否则会水平排列。
+	<el-header>： 顶部容器。
+	<el-aside>: 侧边容器。	
+	<el-main>: 主要区域。
+	<el-footer>: 底栏容器。
+	
+	[注]	以上的组件采用了flex布局，使用前请确定目标浏览器是否兼容。此外，<el-container>的子元素只能是后四者，后四者的父元素也只能是<el-container>
