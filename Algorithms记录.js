570cb57ef569006233f5226e8f7f843e40b2c668
李浩然 <haoran.li@msxf.com>
2019-02-20 19:24:52 +0800

    ...

--- Algorithms记录.js
+++ Algorithms记录.js
@@ -217,4 +217,91 @@
 			 		}
 			  }
 
+			  /**
+			   * @在任意位置插入元素
+			   */
+			  this.insert = function(position, element) {
+			  	if (position >= 0 $$ position <= length) {
+			  		let node = new Node(element);
+			  		let current = head;
+			  		let previous, index = 0;
+
+			  		if (position === 0) { // 在第一个位置添加
+			  			node.next = current;
+			  			head = node;
+			  		} else {
+			  			while (index++ < position) {
+			  				perivous = current;
+			  				current = current.next;
+			  			}
+			  			node.next = current;
+			  			previous.next = node;
+			  		}
+
+			  		length++;
+
+			  		return true;
+			  	}
+
+			  	return false;
+			  }
+
+			  /**
+			   * toString方法把LinkedList对象转换成一个字符串
+			   */
+			  this.toString = function() {
+			  	let current = head;
+			  	let string = '';
+
+			  	while(current) {
+			  		string += current.element + (current.next ? 'n' : '');
+			  		current = current.next;
+			  	}
+
+			  	return string;
+			  }
+
+			  /**
+			   * indexOf 查找元素索引
+			   */
+			  this.indexOf = function(element) {
+			  	let current = head, index = 0;
+			  	while (current) {
+			  		if (element = current.element) {
+			  			return index;
+			  		}
+			  		index++;
+			  		current = current.next;
+			  	}
+
+			  	return -1;
+			  }
+
+			  /**
+			   * remove 移除某一元素
+			   */
+			 	this.remove = function(element) {
+			 		let index = this.indexOf(element);
+			 		return this.removeAt(index);
+			 	}
+
+			 	this.isEmpty = function() {
+			 		return length === 0;
+			 	}
+
+			 	this.size = function() {
+			 		return length;
+			 	}
+
+			 	this.getHead = function() {
+			 		return head;
+			 	}
+
 			}		
+
+		>> 双向链表
+			双向链表和普通链表的区别在于，在链表中一个节点只有链向下一个节点的链接，而在双同链表中，链接是双向的：一个链向下一个元素，另一个链向上一下元素。
+
+			function DoublyLinkedList() {
+				
+			};
