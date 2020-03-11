450b598d3b066de9305b7c103a1f81ab9ae660b3
haoran.li <289002199@qq.com>
2020-03-12 00:25:47 +0800

    config

---
+++ Webpack/基础配置/webpack.config.js
@@ -0,0 +1,125 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const UglifyJsPlugin = require('uglify-js-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+  mode: 'development', // 模式 默认有两种 production development
+  entry: './src/index.js', // 入口
+  output: { // 文件出口
+    filename: 'bundle.[hash:8].js',  // 打包后的文件名  8位hash
+    path: path.resolve(__dirname, 'build') // 路径必须是一个绝对路径
+  },
+  devServer: {  // 开发服务器配置
+    port: 3000,
+    progress: true,
+    contentBase: './build',  // 静态文件夹
+    compress: true
+  },
+  optimizarion: {  // 优化项
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parellel: true,
+        sourceMap: true
+      }),
+      new OptimizeCssAssetsWebpackPlugin()
+    ] 
+  },
+  modules: {  // loader
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'eslint-loader',
+          options: {
+            enforce: "pre" // previous 这个loader会比下一个loader先执行 
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: { // 用babel-loader 需要把es6->es5
+            presets: [
+              '@babel/preset-env'
+            ],
+            plugins: [
+              ["@babel/plugin-proposal-decorators", {legacy: true}],
+              ["@babel/plugin-proposal-class-properties", {loose: true}],
+              "@babel/plugin-transform-runtime"
+            ]
+          }
+        },
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/
+      },
+      // css-loader 解译@import这种语法的
+      // style-loader 把css插入到head的标签中
+      {
+        test: /\.css$/, use: [
+          {
+            loader: 'style-loader',
+            options: {
+              insertAt: 'top'
+            }
+          },
+          'css-loader'
+        ]
+      },
+
+      /* 需要 less 和less-loader 两个组件 */
+      {test: /\.less$/, less: [
+        MiniCssExtractPlugin.loader, 
+        'css-loader',
+        'postcss-loader',
+        'less-loader'
+      ]},
+
+    ]
+  },
+  resolve: { // 解析
+    extensions: ['.js', '.vue', '.json', '.less'],
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  plugins: [ // 插件
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html'),
+      file: 'index.html',
+      minify: {
+        removeAttributeQuotes: true, // 删除html中的双引号
+        collapseInlineTagWhitespace: true, // 折叠空行，即把html变成一行
+      },
+      hash: true
+    }),
+    new webpack.DefinePlugin({
+      __VERSION__: JSON.stringify(appVersion),
+      __FLAG__: JSON.stringify(process.env.FLAG)
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'main.css'
+    }),
+    new webpackk.ProvidePlugion({ // 在每个模块中都注入lodash 
+      _: 'lodash'
+    })
+  ],
+  externals: {
+    _: 'lodash'   // 文件不会被打包
+  }
+}
+
+/**
+ * mini-css-extract-plugin 将css样式从文件中抽离出来
+ * postcss-loader autoprefixer css样式自动加上前缀
+ * babel-loader @babel/core @babel/preset-env
+ * @babel/plugin-transform-runtime @babel-runtime
+ * @babel/polyfill
+ * @expose-loader 暴露全局的loader内联的loader 
+ * pre 前执行的loader / normal 普通loader / 内联loader / 后置loader 
+ */
+import $ from 'expose-loader?$!jquery'
