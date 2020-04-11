ba65bd747f87f8041e5f39308b1a4f04d18a0feb
haoran.li <289002199@qq.com>
2020-04-11 20:37:19 +0800

    ...

--- node/global..js
+++ node/global..js
@@ -4,7 +4,7 @@
  * process.argv 当前进程执行时所带的参数 默认是数组类型的，参数前两个是固定的
  * 1. 当前node的执行命令文件
  * 2. 当前执行的文件是谁 node+文件 执行时，可以传递参数，这些参数会放到数组的第三项
- * process.cwd() 当前里程执行的工作目录
+ * process.cwd() 当前进程执行的工作目录
  * process.env 当前进程的环境变量 会用来区分各种环境 => cross-env
  * process.nextTick node事件环
  */
