31f15128594958b840d2df7f14324895ad10d509
water.li <water.li@saxofintech.com>
2020-10-30 17:10:06 +0800

    ...

--- 计算机基础/数据库/mongo/mongoose.js
+++ 计算机基础/数据库/mongo/mongoose.js
@@ -1,15 +1,36 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/user')
-
-const UserSchema = mongoose.Schema({
-  name: String,
-  age: Number,
-  status: Number
+mongoose.connect('mongodb://127.0.0.1:27017/user', {useNewUrlParser: true}, err => {
+  if (err) {
+    return console.log(err)
+  }
+  console.log('数据库连接成功！')
 })
 
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    trim: true  // ! 数据库保存值会去掉前后空格
+  },
+  age: Number,
+  status: {
+    type: Number,
+    default: 1
+  },
+  param: {
+    type: String,
+    set(value) {
+      return value  // !  这里就是数据库实际保存的值
+    }
+  }
+})
+
+UserSchema.statics.[methods] // todo 添加静态方法，this指向UserSchema
+
+UserSchema.methods.[methods] // todo 添加实例方法, this指向
+
 // 定义数据库模型
-let User = mongoose.model('User', UserSchema, [collection])
+let UserModel = mongoose.model('User', UserSchema, [collection])
 
 // todo 查询User表的数据
 User.find({}, (err, doc) => {
@@ -17,7 +38,7 @@ User.find({}, (err, doc) => {
 })
 
 // todo 增加数据
-let u = new User({
+let u = new UserModel({
   name: '',
   age: 20,
   status: 1
@@ -34,3 +55,17 @@ User.updateOne(
 
 // todo 删除数据
 User.deleteOne({'_id': 'ifg4uiufv'}, (err, doc) => {})
+
+// todo 设置索引
+const DeviceSchema = new mongoose.Schema({
+  sn: {
+    type: String,
+    // ! 唯一索引
+    unique: true
+  },
+  name: {
+    type: String,
+    // ! 普通索引
+    index: true
+  }
+})
