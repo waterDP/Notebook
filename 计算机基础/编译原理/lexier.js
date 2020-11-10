9c61942cdcb5db78181402cdaaa9dfcdf96e051d
water.li <water.li@saxofintech.com>
2020-11-10 17:44:32 +0800

    compiler

--- 计算机基础/编译原理/lexier.js
+++ 计算机基础/编译原理/lexier.js
@@ -1,26 +1,43 @@
 /** 
  * 分词 
- * 状态机
+ * 有限状sj vi态j
  */
 const tokens = []
 
 const NUMBERS = /[0-9]/
+
+const Numeric = 'Numeric'
+const Punctuator = 'Punctuator'
 
 /** 
  * start表示开始状态函数
  * 它是一个函数，接收一个字符，返回下一个状态函数 
  */
 let currentToken
+
+// 确定一个新的token
+function emit(token) {
+  tokens.push(token)
+}
+
 function start(char) {
-  if (NUMBERS.test(char)) {
-    currentToken = {type: 'Number', value: ''}
+  if (NUMBERS.test(char)) {  // 如果这个char是一个数字的话
+    currentToken = {type: Numeric, value: ''}
   }
-  // 进入新状态
+  // 进入新状态 -- 收集捕获Number状态
   return number(char)
 }
 
 function number(char) {
-  
+  if (NUMBERS.test(char)) {  // 如果这个char是一个数字的话
+    currentToken.value += char
+    return number
+  } else if (char === '+' || char === '-') {
+    emit(currentToken)
+    emit({type: Punctuator, value: char})
+    currentToken = {type: Numeric, value: ''}
+    return number
+  }
 }
 
 
@@ -29,8 +46,8 @@ function tokenizer(input) {
   for (let char of input) {
     state = state(char)
   }
+  currentToken.value && emit(currentToken)
 }
 
 tokenizer('10+20')
-
-
+console.log(tokens)
---
+++ 计算机基础/编译原理/src/tokenTypes.js
@@ -0,0 +1,9 @@
+module.exports = {
+  LeftParentheses: 'LeftParentheses',
+  RightParentheses: 'RightParentheses',
+  JSXIdentifier: 'JSXIdentifier',
+  AttributeKey: 'AttributeKey',
+  AttributeStringValue: 'AttributeStringValue',
+  JSXText: 'JSXText',
+  BackSlash: 'BackSlash'
+}
