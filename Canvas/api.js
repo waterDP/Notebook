6e3fde8e7335aee9f300384c67f5f041c6f35e2b
Water Li (李浩然) <water.li@saxofintech.com>
2021-02-03 14:05:29 +0800

    ........

--- Canvas/api.js
+++ Canvas/api.js
@@ -4,20 +4,45 @@
 const theCanvas = document.getElementById('canvas')
 const context  = theCanvas.getContext('2d')
 
-// fillStyle()设置填充颜色
+/**
+ * todo 全局属性
+ */
+// ! 填充颜色
 context.fillStyle = '#ffffaa'
+// ! 边框颜色
+context.strokeStyle = '#ffffaa'
+// ! 透明度
+context.globalAlpha = alpha
+// ! 字体大小和字体
+context.font = '20px Sans-Serif'
+// ! 字体的垂直对齐方式
+context.textBaseline = 'top'
+// ! 字体的垂直对齐方式
+context.textBaseline = 'top'
+// ! 阴影相关
+context.shadowBlur
+context.shadowColor
+context.shadowOffsetX
+context.shadowOffsetY
 
-// 绘制一个方块
-context.fillReact(0, 0, 500, 300)
+// ! 线的粗细
+context.lineWidth = 10
+
+
+// todo 当前状态的保存与恢复
+context.save()
+context.restore()
 
 /**
- *  todo 字体
+ *  todo 绘制图像
  */
-// 设置字体大小和字体
-context.font = '20px Sans-Serif'
 
-// 设置字体的垂直对齐方式
-context.textBaseline = 'top'
+// 绘制一个方块
+context.fillReact(x, y, width, height)
+// 描边一个方块
+context.strokeRect(x, y, width, height)
+// 清除一个方块内的内容
+context.clearRect(x, y, width, height)
 
 // 绘制文字
 context.fillText('text', x, y, maxWidth)
@@ -28,3 +53,55 @@ img.onload = () => {
   context.drawImage(img, dX, dY, dWidth, dHeight)
 }
 img.src = 'img.gif'
+
+
+/**
+ * todo 绘制路径
+ */
+/**
+ * @description: 弧线 
+ * @param {*} xy 定义圆心
+ * @param radius 定义弧线半径
+ * @param startAngle endAngle 开始与结束角度 弧度缺点
+ * @param anticlockwise 方向
+ */
+context.arc(x, y, radius, Math.PI / 180 * startAngle, Math.PI / 180 * endAngle, anticlockwise)
+
+context.arcTo(x1, y1, x2, y2, radius);
+
+
+// 简单直线路径
+function drawScreen() {
+  context.strokeStyle = 'black'
+  context.lineWidth= 10
+  context.lineCap='square'
+  context.beginPath()
+  context.moveTo(20, 0)
+  context.lineTo(100, 0)
+  context.stroke() // 描边
+  context.closePath()
+}
+
+// ! 贝塞尔曲线
+context.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y);
+context.quadraticCurveTo(cpx, cpy, x, y);
+
+
+// todo 渐变
+context.createLinearGradient(x1, y1, x2, y2)  //  线性渐变
+context.createRadialGradient(x1, y1, r1, x2, y2, r2) // 径向渐变
+
+// todo 渐变色设置
+// 水平渐变
+function drawScreen() {
+  let gr = context.createLinearGradient(0, 0, 100, 0)
+  // 添加颜色点
+  gr.addColorStop(0, 'rgb(255,0,0)');
+  gr.addColorStop(0.5, 'rgb(0,255,0)')
+  gr.addColorStop(1, 'rgb(0,0,255)')
+  // 应用fillStyle生成渐变
+  context.fillStyle = gr
+  context.fillReact(0, 0, 100, 100)
+}
+
+
