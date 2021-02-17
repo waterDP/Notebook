36488c8a2fbac73dd67ddd5b5c8ae6b828ad20ce
water.li <water.li@saxofintech.com>
2021-02-17 22:12:07 +0800

    ...

--- 计算机基础/算法分析/双指针.js
+++ 计算机基础/算法分析/双指针.js
@@ -1,1 +1,2 @@
-// leetcode 167
+// leetcode 167  两个数之和
+// leetcode 633  平方数之和
---
+++ 计算机基础/算法案例/633.平方数之和.js
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode.cn id=633 lang=javascript
+ *
+ * [633] 平方数之和
+ *
+ * https://leetcode-cn.com/problems/sum-of-square-numbers/description/
+ *
+ * algorithms
+ * Medium (35.00%)
+ * Likes:    163
+ * Dislikes: 0
+ * Total Accepted:    37.3K
+ * Total Submissions: 106.4K
+ * Testcase Example:  '5'
+ *
+ * 给定一个非负整数 c ，你要判断是否存在两个整数 a 和 b，使得 a^2 + b^2 = c 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：c = 5
+ * 输出：true
+ * 解释：1 * 1 + 2 * 2 = 5
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：c = 3
+ * 输出：false
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 输入：c = 4
+ * 输出：true
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 输入：c = 2
+ * 输出：true
+ * 
+ * 
+ * 示例 5：
+ * 
+ * 输入：c = 1
+ * 输出：true
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 0 <= c <= 2^31 - 1
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number} c
+ * @return {boolean}
+ */
+var judgeSquareSum = function (c) {
+  let i = 0, j = Math.floor(Math.sqrt(c))
+  while (i <= j) {
+    let sum = i * i + j * j
+    if (sum === c) {
+      return true
+    }
+    if (sum < c) {
+      i += 1
+    } else {
+      j -= 1
+    }
+  }
+  return false
+};
+
+judgeSquareSum(2)
+// @lc code=end
+
