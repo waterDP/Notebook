dec32fa17f2f99f9a104acdba78e54e33f6690a5
Water Li (李浩然) <water.li@saxofintech.com>
2021-02-18 15:07:53 +0800

    ...

--- 计算机基础/算法分析/串匹配.js
+++ 计算机基础/算法分析/串匹配.js
@@ -1,6 +1,7 @@
 // ! KMP
 function kmpSearch(s, p) {
   let i = 0, j = 0, sLen = s.length, pLen = p.length
+  let next = getNext(p)
   while (i < sLen && j < pLen) {
     // 如果j=-1，或者当前字符匹配成功（即s[i] === p[j]）, 者令i++, j++
     if (j === -1 || s[i] === p[j]) {
@@ -15,3 +16,21 @@ function kmpSearch(s, p) {
   }
   return -1
 }
+function getNext(p) {
+  let pLen = p.length
+  let k = -1, j = 0
+  const res = [-1]
+  while(j < pLen - 1) {
+    // p[k] 表示前缀 p[j]表示后缀
+    if (k == -1||p[j] === p[k]) {
+      ++k
+      ++j
+      res[j] = k
+    } else  {
+      k = res[k]
+    }
+  }
+  return res
+}
+
+console.log(kmpSearch('aababaabf', 'abf'))
