58f80a4259ecec56092b4a8d38432c27ca9fa63e
water.li <water.li@saxofintech.com>
2021-02-19 20:05:45 +0800

    ...

--- 计算机基础/算法案例/40.组合总和-ii.js
+++ 计算机基础/算法案例/40.组合总和-ii.js
@@ -11,24 +11,26 @@
  * @return {number[][]}
  */
 var combinationSum2 = function(candidates, target) {
-  const ans = []
-  const dfs = (target, path, candidates) => {
-    const [first, ...rest] = candidates
-    const current = target - first
-    if (current === 0) {
-      ans.push([...path, first])
-      return
+  candidates.sort((a, b) => a - b)
+  const res = []
+  const dfs = (start, temp, sum) => {
+    if (sum >= target) {        // 爆掉了，不用继续选数了
+      if (sum == target) {      // 加入解集
+        res.push(temp.slice())  // temp的拷贝
+      }
+      return                   // 结束当前递归
     }
-    if (current > 0) {
-      dfs(current, [...path, first], rest)
-    } else {
-      dfs(target, path, rest)
+    for (let i = start; i < candidates.length; i++) { // 枚举出选择，从start开始
+      if (i - 1 >= start && candidates[i - 1] == candidates[i]) {
+        continue
+      }
+      temp.push(candidates[i])        // 加入“部分解”
+      dfs(i+1, temp, sum + candidates[i]) // 往下继续选择，同时sum累加上当前数字
+      temp.pop()                      // 撤销选择
     }
   }
-  dfs(target, [], candidates)
-  return ans
+  dfs(0, [], 0)
+  return res
 };
-
-console.log(combinationSum2([1, 2, 7, 6, 1, 5], 8))
 // @lc code=end
 
