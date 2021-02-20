e7d0c5e672fddd32c3254645a0eb87f49d90c149
Water Li (李浩然) <water.li@saxofintech.com>
2021-02-20 14:43:31 +0800

    ...

--- 计算机基础/算法案例/69.x-的平方根.js
+++ 计算机基础/算法案例/69.x-的平方根.js
@@ -10,20 +10,20 @@
  * @return {number}
  */
 const mySqrt = (x) => {
-  if (x < 2) return x;
-  let left = 1;
-  let right = x >>> 1;        // 除以2并取整，缩小一下遍历的范围
+  if (x < 2) return x
+  let left = 1
+  let right = parseInt(x / 2)      // 除以2并取整，缩小一下遍历的范围
   while (left + 1 < right) {  // 退出循环时，它们相邻
-    let mid = (left + right) >>> 1;
+    let mid = parseInt((left + right)/2)
     if (mid == x / mid) {
-      return mid;
+      return mid
     } else if (mid < x / mid) {
-      left = mid;
+      left = mid
     } else {
-      right = mid;
+      right = mid
     }
   }
-  return right > x / right ? left : right;
+  return right > x / right ? left : right
 }
 // @lc code=end
 
