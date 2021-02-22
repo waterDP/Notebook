3b9701aa398093625cb3110aadbfa261958e3a41
water.li <water.li@saxofintech.com>
2021-02-22 12:19:59 +0800

    ...

---
+++ 计算机基础/算法案例/78.子集.js
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode.cn id=78 lang=javascript
+ *
+ * [78] 子集
+ *
+ * https://leetcode-cn.com/problems/subsets/description/
+ *
+ * algorithms
+ * Medium (79.53%)
+ * Likes:    999
+ * Dislikes: 0
+ * Total Accepted:    197.8K
+ * Total Submissions: 248.7K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+ * 
+ * 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [1,2,3]
+ * 输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = [0]
+ * 输出：[[],[0]]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 
+ * -10 
+ * nums 中的所有元素 互不相同
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+  let res = [[]]
+  for (let i = 0; i < nums.length; i++) {
+    let len = res.length
+    for (let j = 0; j < len; j++) {
+      res.push([...res[j], nums[i]])
+    }
+  }
+  return res
+};
+subsets([1,2,3])
+// @lc code=end
+
