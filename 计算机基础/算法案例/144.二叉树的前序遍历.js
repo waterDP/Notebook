714857340ce99122d1544031d9548307d7149c3c
water.li <water.li@saxofintech.com>
2021-02-24 15:53:44 +0800

    ...

---
+++ 计算机基础/算法案例/144.二叉树的前序遍历.js
@@ -0,0 +1,96 @@
+/*
+ * @lc app=leetcode.cn id=144 lang=javascript
+ *
+ * [144] 二叉树的前序遍历
+ *
+ * https://leetcode-cn.com/problems/binary-tree-preorder-traversal/description/
+ *
+ * algorithms
+ * Medium (69.14%)
+ * Likes:    514
+ * Dislikes: 0
+ * Total Accepted:    262.4K
+ * Total Submissions: 379.5K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * 给你二叉树的根节点 root ，返回它节点值的 前序 遍历。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [1,null,2,3]
+ * 输出：[1,2,3]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = []
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = [1]
+ * 输出：[1]
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 
+ * 输入：root = [1,2]
+ * 输出：[1,2]
+ * 
+ * 
+ * 示例 5：
+ * 
+ * 
+ * 输入：root = [1,null,2]
+ * 输出：[1,2]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点数目在范围 [0, 100] 内
+ * -100 
+ * 
+ * 
+ * 
+ * 
+ * 进阶：递归算法很简单，你可以通过迭代算法完成吗？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+  const list = []
+  const stack = []
+  root && stack.push(root)
+  while(stack.length) {
+    const curNode = stack.pop()
+    list.push(curNode.val)
+    curNode.right && stack.push(curNode.right)
+    curNode.left && stack.push(curNode.left)
+  }
+  return list
+};
+// @lc code=end
+
---
+++ 计算机基础/算法案例/145.二叉树的后序遍历.js
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=145 lang=javascript
+ *
+ * [145] 二叉树的后序遍历
+ *
+ * https://leetcode-cn.com/problems/binary-tree-postorder-traversal/description/
+ *
+ * algorithms
+ * Medium (74.12%)
+ * Likes:    529
+ * Dislikes: 0
+ * Total Accepted:    189.8K
+ * Total Submissions: 255.9K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * 给定一个二叉树，返回它的 后序 遍历。
+ * 
+ * 示例:
+ * 
+ * 输入: [1,null,2,3]  
+ * ⁠  1
+ * ⁠   \
+ * ⁠    2
+ * ⁠   /
+ * ⁠  3 
+ * 
+ * 输出: [3,2,1]
+ * 
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function(root) {
+  
+};
+// @lc code=end
+
