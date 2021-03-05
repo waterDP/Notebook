aac52f96c49fc766d1d5f9e240259fea70a30b17
Water Li (李浩然) <water.li@saxofintech.com>
2021-03-05 10:07:56 +0800

    ...

---
+++ 计算机基础/算法案例/328.奇偶链表.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=328 lang=javascript
+ *
+ * [328] 奇偶链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function(head) {
+    
+};
+// @lc code=end
+
