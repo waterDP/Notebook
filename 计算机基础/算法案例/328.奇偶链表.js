479275da60117c195cdd5e07d5f9a271ea42a2a5
water.li <water.li@saxofintech.com>
2021-03-06 16:43:15 +0800

    ...

--- 计算机基础/算法案例/328.奇偶链表.js
+++ 计算机基础/算法案例/328.奇偶链表.js
@@ -17,7 +17,18 @@
  * @return {ListNode}
  */
 var oddEvenList = function(head) {
-    
-};
-// @lc code=end
+  if (head === null) {
+    return head
+  }
+  let evenHead = head.next
+  let odd = head, even = evenHead
+  while(even!== null && even.next !== null){
+    odd.next = even.next
+    odd = odd.next
+    even.next = odd.next
+    even = even.next
+  }
+  odd.next = evenHead
+  return head
+}
 
