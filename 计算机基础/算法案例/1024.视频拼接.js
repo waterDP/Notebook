cdf582c70da05a5199e95406e72c0ba5b206e577
Water Li (李浩然) <water.li@saxofintech.com>
2021-03-19 11:25:00 +0800

    ...

--- 计算机基础/算法案例/1024.视频拼接.js
+++ 计算机基础/算法案例/1024.视频拼接.js
@@ -11,27 +11,26 @@
  * @param {number} T
  * @return {number}
  */
-var videoStitching = function(clips, T) {
-  let pre = 0;
-  let res = 0;
-  let last = 0;
-  let maxEnd = new Array(T).fill(false);
+var videoStitching = function (clips, T) {
+  let pre = res = last = 0
+  let maxEnd = new Array(T).fill(false)
+
   clips.forEach(item => {
-      if(item[0] < T){
-          maxEnd[item[0]] = Math.max(maxEnd[item[0]], item[1]);
-      }
+    if (item[0] < T) {
+      maxEnd[item[0]] = Math.max(maxEnd[item[0]], item[1])
+    }
   })
-  for(let i = 0; i < T; i++){
-      last = Math.max(maxEnd[i], last);
-      if(i === last){
-          return -1;
-      }
-      if(i === pre){
-          res++;
-          pre = last;
-      }
+
+  for (let i = 0; i < T; i++) {
+    last = Math.max(maxEnd[i], last)
+    if (i === last) {
+      return -1
+    }
+    if (i === pre) {
+      res++
+      pre = last
+    }
   }
   return res
 };
 // @lc code=end
-
