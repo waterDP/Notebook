ba750076b5ef2716592c5fad00c8b78250e3d250
Water Li (李浩然) <water.li@saxofintech.com>
2021-03-19 09:21:55 +0800

    ...

---
+++ 计算机基础/算法案例/206.反转链表.js
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=javascript
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * todo 方法一 迭代
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+  let prev = null
+  let curr = head 
+  while(curr) {
+    const next = curr.next
+    curr.next = prev
+    prev = curr
+    curr = next
+  }
+  return prev
+};
+
+
+/**
+ * todo 方法二 递归
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+  if (head == null || head.next == null) {
+    return head
+  }
+  const newHead = reverseList(head.next)
+  head.next.next = head
+  head.next = null
+  return newHead
+}
+// @lc code=end
+
