80e92c6b2e9ebb7642702884d7a887104e1e64bd
Water Li (李浩然) <water.li@saxofintech.com>
2021-03-24 17:46:35 +0800

    ..

---
+++ 计算机基础/算法案例/136.只出现一次的数字.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=136 lang=javascript
+ *
+ * [136] 只出现一次的数字
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+  let res = 0
+  for (let n of nums) {
+    res ^= n
+  }
+  return res
+};
+// @lc code=end
+
