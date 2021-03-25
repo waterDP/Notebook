0ea7f7d651c48c06ecfc99af03e2455bc9c43260
Water Li (李浩然) <water.li@saxofintech.com>
2021-03-25 13:38:01 +0800

    ..

---
+++ 计算机基础/算法案例/86.分隔链表.js
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=86 lang=javascript
+ *
+ * [86] 分隔链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+var partition = function(head, x) {
+  if (!head) return head
+  let small = new ListNode(0)
+  let large = new ListNode(0)
+  let smallH = small
+  let largeH = large
+  while (head) {
+    if (head.val < x) {
+      small.next = head
+      small = small.next
+    } else {
+      large.next = head
+      large = large.next
+    }
+    head = head.next
+  }
+  large.next = null
+  small.next = largeH.next
+  return smallH.next
+};
+// @lc code=end
+
