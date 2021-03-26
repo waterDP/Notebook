58a646a81c9b3e93e62642f784b3df1240226478
Water Li (李浩然) <water.li@saxofintech.com>
2021-03-26 15:27:59 +0800

    ..

---
+++ 计算机基础/算法案例/237.删除链表中的节点.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=237 lang=javascript
+ *
+ * [237] 删除链表中的节点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+  node.val = node.next.val
+  node.next = node.next.next
+};
+// @lc code=end
+
