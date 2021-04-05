8a40f4325e789809bbefeb2f1d9435bd3af4e654
water.li <water.li@saxofintech.com>
2021-04-05 11:13:59 +0800

    ...

---
+++ 计算机基础/算法案例/103.二叉树的锯齿形层序遍历.js
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode.cn id=103 lang=javascript
+ *
+ * [103] 二叉树的锯齿形层序遍历
+ *
+ * https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal/description/
+ *
+ * algorithms
+ * Medium (57.11%)
+ * Likes:    426
+ * Dislikes: 0
+ * Total Accepted:    127.4K
+ * Total Submissions: 223.1K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，返回其节点值的锯齿形层序遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+ * 
+ * 例如：
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ * 
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 
+ * 返回锯齿形层序遍历如下：
+ * 
+ * 
+ * [
+ * ⁠ [3],
+ * ⁠ [20,9],
+ * ⁠ [15,7]
+ * ]
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function(root) {
+  const ret = []
+  const helper = (node, level) => {
+    if (node) {
+      if(ret[level]) {
+        level%2 === 0
+          ? ret[level].push(node.val)
+          : ret[level].unshift(node.val)
+      } else {
+        ret[level] = [node.val]
+      }
+      helper(node.left, level+1)
+      helper(node.right, level+1)
+    }
+  }
+  helper(root, 0)
+  return ret
+};
+// @lc code=end
+
