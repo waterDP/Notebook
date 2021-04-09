2e078ac0dfd1188090c403df2dc9ec150fff4893
Water Li (李浩然) <water.li@saxofintech.com>
2021-04-09 17:21:36 +0800

    ..

--- 计算机基础/算法题集/300.最长递增子序列.js
+++ 计算机基础/算法题集/300.最长递增子序列.js
@@ -10,7 +10,19 @@
  * @return {number}
  */
 var lengthOfLIS = function(nums) {
+  const length = nums.length
+  const dp = new Array(length)
+  dp.fill(1)
+  let result = 0
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < i; j++) {
+      (nums[i] > nums[j]) &&
+      (dp[i] = Math.max(dp[i], dp[j] + 1))
+    }
+    result = Math.max(result, dp[i])
+  }
 
+  return result
 };
 // @lc code=end
 
