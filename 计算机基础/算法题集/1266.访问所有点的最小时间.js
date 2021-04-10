4db9c672812482d233458c785515ed704108262c
water.li <water.li@saxofintech.com>
2021-04-10 22:28:42 +0800

    ..

--- 计算机基础/算法题集/1266.访问所有点的最小时间.js
+++ 计算机基础/算法题集/1266.访问所有点的最小时间.js
@@ -64,7 +64,7 @@
  * dx < dy：沿对角线移动 dx 次，再竖直移动 dy - dx 次，总计 dx + (dy - dx) = dy 次；
  * dx == dy：沿对角线移动 dx 次；
  * dx > dy：沿对角线移动 dy 次，再水平移动 dx - dy 次，总计 dy + (dx - dy) = dx 次。
- *可以发现，对于任意一种情况，从 x 移动到 y 的最少次数为 dx 和 dy 中的较大值 max(dx, dy)，这也被称作 x 和 y 之间的 切比雪夫距离。
+ * 可以发现，对于任意一种情况，从 x 移动到 y 的最少次数为 dx 和 dy 中的较大值 max(dx, dy)，这也被称作 x 和 y 之间的 切比雪夫距离。
  */
 
 // @lc code=start
