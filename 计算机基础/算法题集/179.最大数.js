b517cb471f665bee33492944b9c7b6f7a0fdf6d3
Water Li (李浩然) <water.li@saxofintech.com>
2021-04-12 13:39:34 +0800

    .

---
+++ 计算机基础/算法题集/179.最大数.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=179 lang=javascript
+ *
+ * [179] 最大数
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var largestNumber = function(nums) {
+  nums.sort((x, y) => {
+    let sx =10, sy =10
+    while(sx <= x) {
+      sx *= 10
+    }
+    while(sy <= y) {
+      sy *= 10
+    }
+    return (sx * y + x).toString() - (sy * x + y).toString()
+  })
+  if (nums[0] === 0) return '0'
+  return nums.join('')
+};
+// @lc code=end
+
