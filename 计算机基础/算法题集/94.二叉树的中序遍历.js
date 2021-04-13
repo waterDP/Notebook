52e1c96dc9e4eeee648ec2acb2b509f0c3d09869
Water Li (李浩然) <water.li@saxofintech.com>
2021-04-13 13:23:33 +0800

    ..

---
+++ 计算机基础/算法题集/94.二叉树的中序遍历.js
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=javascript
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * todo morris 算法
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+  const res = []
+  let predecessor = null
+
+  while (root) {
+    if (root.left) {
+      predecessor = root.left
+      while (predecessor.right && predecessor.right !== root) {
+        predecessor = predecessor.right
+      }
+
+      if (!predecessor.right) {
+        predecessor.right = root;
+        root = root.left
+      } else {
+        res.push(root.val)
+        predecessor.right = null
+        root = root.right
+      }
+    }
+    else {
+      res.push(root.val)
+      root = root.right
+    }
+  }
+  return res
+}
+// @lc code=end
+
