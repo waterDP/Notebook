ab34141ad9382ec5e43f07e9e68609611acd446e
Water Li (李浩然) <water.li@saxofintech.com>
2021-04-14 20:12:14 +0800

    ..

--- 计算机基础/算法题集/509.斐波那契数.js
+++ 计算机基础/算法题集/509.斐波那契数.js
@@ -19,31 +19,31 @@ var fib = function (n) {
 // todo 矩阵快速幂 T=O(log n) S=O(1)
 var fib = function (n) {
   if (n < 2) {
-    return n;
+    return n
   }
-  const q = [[1, 1], [1, 0]];
-  const res = pow(q, n - 1);
-  return res[0][0];
+  const q = [[1, 1], [1, 0]]
+  const res = pow(q, n - 1)
+  return res[0][0]
 };
 
 const pow = (a, n) => {
-  let ret = [[1, 0], [0, 1]];
+  let ret = [[1, 0], [0, 1]]
   while (n > 0) {
     if ((n & 1) === 1) {
-      ret = multiply(ret, a);
+      ret = multiply(ret, a)
     }
-    n >>= 1;
-    a = multiply(a, a);
+    n >>= 1
+    a = multiply(a, a)
   }
-  return ret;
+  return ret
 }
 
 const multiply = (a, b) => {
   const c = new Array(2).fill(0).map(() => new Array(2).fill(0));
   for (let i = 0; i < 2; i++) {
     for (let j = 0; j < 2; j++) {
-      c[i][j] = a[i][0] * b[0][j] + a[i][1] * b[1][j];
+      c[i][j] = a[i][0] * b[0][j] + a[i][1] * b[1][j]
     }
   }
-  return c;
+  return c
 }
