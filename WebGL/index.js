7c81f8137d49ab8597d9603b84573dadcd18a052
Water Li (李浩然) <water.li@saxofintech.com>
2021-04-28 08:38:29 +0800

    .

--- WebGL/index.js
+++
@@ -1,9 +0,0 @@
-// HelloCanvas
-function main() {
-  const canvas = document.getElementById('webgl')
-
-  const gl = getWebGLContext(canvas)
-
-  gl.clearColor(0.0, 0.0, 0.0, 1.0)
-  gl.clear(gl.COLOR_BUFFER_BIT)
-}
