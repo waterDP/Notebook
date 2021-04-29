1bc2d04eb40dd39bda67d802f9f21676d0bc6b7e
Water Li (李浩然) <water.li@saxofintech.com>
2021-04-29 10:11:01 +0800

    ..

--- 计算机基础/算法题集/403.青蛙过河.js
+++ 计算机基础/算法题集/403.青蛙过河.js
@@ -14,18 +14,18 @@ var canCross = function (stones) {
   const dp = new Array(n).fill(0).map(() => new Array(n).fill(0))
   dp[0][0] = true
   for (let i = 1; i < n; ++i) {
-    if (stones[i] - stones[i-1] > i) {
+    if (stones[i] - stones[i - 1] > i) {
       return false
     }
   }
   for (let i = 1; i < n; i++) {
-    for (let j = i-1; j >= 0; j--) {
+    for (let j = i - 1; j >= 0; j--) {
       const k = stones[i] - stones[j]
-      if (k > j+ 1) {
+      if (k > j + 1) {
         break
       }
-      dp[i][k] = dp[j][k-1]||dp[j][k]||dp[j][k+1]
-      if (i === n-1 && dp[i][k]) {
+      dp[i][k] = dp[j][k - 1] || dp[j][k] || dp[j][k + 1]
+      if (i === n - 1 && dp[i][k]) {
         return true
       }
     }
