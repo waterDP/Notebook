bc7fb3f5e48759e52edf15144894d4cf2a2a46ef
Water Li (李浩然) <water.li@saxofintech.com>
2021-05-20 14:16:23 +0800

    .

---
+++ 计算机基础/算法题集/1798.你能构造出连续值的最大数目.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=1798 lang=javascript
+ *
+ * [1798] 你能构造出连续值的最大数目
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} coins
+ * @return {number}
+ */
+var getMaximumConsecutive = function(coins) {
+  coins.sort((a, b) => a - b)
+  debugger
+  let x = 0
+  for (let coin of coins) {
+    if (coin > x+1) {
+      break
+    }
+    x += coin
+  }
+  return x+1
+};
+// @lc code=end
+
