776ec276f784e83aee400915f28c16831aa2e922
water.li <water.li@saxofintech.com>
2021-08-05 15:13:34 +0800

    .

--- node/Koa/src/lib/application.js
+++ node/Koa/src/lib/application.js
@@ -91,7 +91,7 @@ class Application extends EventEmitter {
     })
   }
   listen(...args) {
-    const server = http.createServer(this.handleRequest.bind(this))
+    const server = http.createServer(this.handleRequest)
     server.listen(...args)
   }
 }
