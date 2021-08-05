3b92568e7b55f49b30b23794184cfcf1e08d9e51
water.li <water.li@saxofintech.com>
2021-08-05 22:22:36 +0800

    .

--- React/redux/middleware/redux-promise.js
+++ React/redux/middleware/redux-promise.js
@@ -1,4 +1,4 @@
- function isPromise(obj) {
+function isPromise(obj) {
   return !!obj && (typeof obj == 'object' || typeof obj == 'function') && (typeof obj.then == 'function')
 }
 export default function ({ dispatch, getState }) {
