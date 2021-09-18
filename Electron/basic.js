e6a5996d064cfe7e9287b4e00b39d13ebce0402a
water.li <water.li@saxofintech.com>
2021-09-18 16:13:28 +0800

    .

--- Electron/basic.js
+++ Electron/basic.js
@@ -27,30 +27,50 @@
  */
 
 // todo 在渲染进程中显示通知
-const {Notification} = require('Electron')
 
-const myNotification = new Notification({
-  body: 'Notification from the Renderer process'
-})
-myNotification.onclick = () => {
-  console.log('Notification clicked')
-}
-
-// todo 在主进程中显示通知
+/* ----------index.html------------- */
+`<html>
+  <head>
+    <meta charset="UTF-8">
+    <!-- https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP -->
+    <meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self'">
+    <meta http-equiv="X-Content-Security-Policy" content="default-src 'self'; script-src 'self'">
+    <title>Hello World!</title>
+  </head>
+  <body>
+    <h1>Hello World!</h1>
+    We are using Node.js <span id="node-version"></span>,
+    Chromium <span id="chrome-version"></span>,
+    and Electron <span id="electron-version"></span>.
+  </body>
+</html>`
+/* ----------------------------------- */
 const {app, BrowserWindow, Notification} = require('electron')
 
+//  todo 创建一个窗体，并添加index.html的内容
 function createWindow () {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true
+    },
+    // 预加载
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
     }
   })
 
   win.loadFile('index.html')
 }
 
+// 管理窗口的生命周期
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+
+// todo 在主进程中显示通知
 function showNotification() {
   const notification = {
     title: 'Basic Notification',
@@ -58,11 +78,46 @@ function showNotification() {
   }
   new Notification(notification).show()
 }
-app.whenReady().then(createWindow).then(showNotification)
+app.whenReady().then(() => {
+  createWindow()
+  /**
+   * 因为窗口无法在ready事件前创建，你应当在你的应用初始化后仅监听activate事件
+   * 通过您现有的whenReady()回调中附上您的事件监听器来完成这个操作
+   */
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+}).then(() => {
+  // 显示一个通知
+  showNotification()
+})
+
+
+
+
+// 预加载
+// 预加载脚本连接到渲染器时派上用场的地址，预加载脚本在渲染器进程加载之前加载，
+// 并有权访问两个渲染器全局（如window和document）和node环境
+/* ---------preload.js----------- */
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) {
+      element.innerText = text
+    }
+  }
+  for (const dependency of ['chrome', 'node', 'electron']) {
+    replaceText(`${dependency}-version`, process.versions[dependency])
+  }
+})
+/* ------------------------------ */
+
+
 
 // todo 将一个项目添加到最近文档
 const {app} = require('electron')
-
 app.addRecentDocument('/Users/USERNAME/work.type')
 
 // 清理
