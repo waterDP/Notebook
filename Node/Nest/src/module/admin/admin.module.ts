ca318e6993d9d4a33bd1c679295001a26a38b1d4
water.li <water.li@saxofintech.com>
2021-09-30 09:37:55 +0800

    .

--- Node/Nest/src/module/admin/admin.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MainController } from './main/main.controller';
-import { LoginController } from './login/login.controller';
-import { ManagerController } from './manager/manager.controller';
-
-@Module({
-  controllers: [MainController, LoginController, ManagerController]
-})
-export class AdminModule {}
--- Node/Nest/src/module/admin/manager/manager.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('admin/manager')
-export class ManagerController {
-  @Get()
-  index() {
-    return '我是后台首页'
-  }
-}
