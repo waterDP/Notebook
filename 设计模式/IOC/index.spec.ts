7583aea660acee53f649db507f95baf77e925435
water.li <water.li@saxofintech.com>
2021-10-13 21:52:56 +0800

    .

--- 设计模式/IOC/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import {Container} from './'
-
-let container = new Container()
-const point = {x:10, y: 10}
-class BasicClass{}
-
-container.addProvider({provide: BasicClass, useClass: BasicClass})
-
-console.log(container.providers) 
