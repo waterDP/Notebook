ba51dda493f6b45c25afa672b429b844a929144e
water.li <water.li@saxofintech.com>
2021-10-29 11:00:32 +0800

    .

--- Vue/插件案例/component.js
+++ Vue/插件案例/component.js
@@ -1,11 +1,12 @@
 // 自动加载公共组件
+const fs = require('fs')
 const files = require.context('./', true, /\.vue$/)
 
 const install = Vue => {
   files.keys().forEach(fileName => {
     const componentConfig = files(fileName)
     const component = componentConfig.default || componentConfig
-    const componentName = component.name || fileName.replace(/^.\//, '').replace(/\.js$/, '')
+    const componentName = component.name || fs.baseName(fileName)
     Vue.component(componentName, component)
   })
 }
