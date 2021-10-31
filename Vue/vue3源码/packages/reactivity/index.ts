c50a69eb2b6bb302363666a066cae8adc091ef8c
water.li <water.li@saxofintech.com>
2021-10-31 21:41:01 +0800

    .

--- Vue/vue3源码/packages/reactivity/index.ts
+++ Vue/vue3源码/packages/reactivity/index.ts
@@ -1,8 +1,16 @@
 /*
- * @Author: water.li
+ * @Author: your name
  * @Date: 2021-10-31 19:57:23
- * @LastEditTime: 2021-10-31 19:57:24
+ * @LastEditTime: 2021-10-31 21:40:52
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \notebook\Vue\vue3源码\packages\reactivity\index.ts
  */
+
+
+export {
+  reactive,
+  shallowReactive,
+  readonly,
+  shallowReadonly,
+} from "./reactive"
