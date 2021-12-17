9343ec80948a3604ea5b49865a0258c3a07f4602
li.haoran <li.haoran@99cloud.net>
2021-12-17 14:57:30 +0800

    .

--- JavaScript/深拷贝与浅拷贝.js
+++ JavaScript/深拷贝与浅拷贝.js
@@ -7,12 +7,13 @@
 // 深拷贝 拷贝后的结果更改是不会影响拷贝前的 拷贝前后是没有关系的 
 // 浅拷贝 改变拷贝前的内容， 会对拷贝后的内容有影响 拷贝前后是有关系的
 
-// ! 浅拷贝
+/*
+ * ===================================================
+ *   浅拷贝 
+ * ===================================================
+ */
 
 // todo for...in只循环第一层
-/* 
- * 第一层是独立的 不能完全算浅拷贝 
- */
 function simpleCopy(obj) {
   let ret = Array.isArray(obj) ? [] : {}
   for (let i in obj) {
@@ -29,8 +30,6 @@ let ret = Object.assign(obj)
 let a = [1, 2, 3]
 let ret = a
 
-// ! 深拷贝
-
 // todo 展开运算符
 let obj = { name: 'jwr', address: { x: 100, y: 100 } }
 let o = { ...obj }
@@ -38,12 +37,18 @@ obj.address.x = 200
 console.log(obj, o)
 
 
+/*
+ * ====================================================
+ *    深拷贝
+ * ====================================================
+ */
+
 // todo JSON.parse(JSON.stringify()) 深拷贝
 let obj = { name: 'jwr', address: { x: 100, y: 100 }, f: function () { }, un: undefined }
 let o = JSON.parse(JSON.stringify(obj))
 obj.address.x = 200
 
-/* *
+/** 
  * ! 缺点
  * 1. undefined,任意的函数、正则表达式以及Symbol值，在序列过程中会被忽略
  * 2. 它会抛弃对象的constructor。也就是深拷贝之后，不管这个对象原来的构造函数是什么，在深拷贝之后都会变成Object
@@ -51,11 +56,11 @@ obj.address.x = 200
  */
 
 // todo 结构化克隆算法
-/* 
-* Message Channel 建立两个端，一个端发送消息，另一个端接收消息
-* 这种方法的优点就是能解决循环引用的问题，还支持大量的内置数据类型。
-* 缺点就是这个方法是异步的
-*/
+/** 
+ * Message Channel 建立两个端，一个端发送消息，另一个端接收消息
+ * 这种方法的优点就是能解决循环引用的问题，还支持大量的内置数据类型。
+ * 缺点就是这个方法是异步的
+ */
 
 function structoralClone(obj) {
   return new Promise(resolve => {
@@ -161,7 +166,7 @@ function cloneDeep(value) {
 function cloneDeep(obj) {
   const q = [obj] // q是一个队列结构，实现BFS的基础
   const res = {}
-  const map = new Map()
+  const map = new Map() // 缓存拷贝的值，避免循环引用的问题
   map.set(obj, res)
 
   const isObject = o => Object.prototype.toString.call(o) === '[object object]'
