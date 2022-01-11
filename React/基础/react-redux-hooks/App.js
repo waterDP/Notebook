45262709c9484feaa694188899544d599060780d
li.haoran <li.haoran@99cloud.net>
2022-01-11 22:12:23 +0800

    .

--- React/基础/react-redux-hooks/App.js
+++ React/基础/react-redux-hooks/App.js
@@ -1,29 +1,36 @@
+/*
+ * @Author: water.li
+ * @Date: 2021-10-24 17:09:39
+ * @Description: 
+ * @FilePath: \notebook\React\基础\react-redux-hooks\App.js
+ */
 import React from 'react';
 import SimpleCounter from './views/SimpleCounter';
 import TodoList from './views/TodoList';
 import NameCard from './views/NameCard';
 
+
 function App() {
 
-    return (
-        <div className="App">
-            <main className="container">
+	return (
+		<div className="App">
+			<main className="container">
 
-                {/* Please check /views/SimpleCounter.js for detail*/}
-                <SimpleCounter/>
+				{/* Please check /views/SimpleCounter.js for detail*/}
+				<SimpleCounter />
 
-                <div className="divider"></div>
+				<div className="divider"></div>
 
-                {/* Please check /views/NameCard.js for detail*/}
-                <NameCard/>
+				{/* Please check /views/NameCard.js for detail*/}
+				<NameCard />
 
-                <div className="divider"></div>
+				<div className="divider"></div>
 
-                {/* Please check /views/TodoList.js for detail*/}
-                <TodoList/>
-            </main>
-        </div>
-    );
+				{/* Please check /views/TodoList.js for detail*/}
+				<TodoList />
+			</main>
+		</div>
+	);
 }
 
 export default App;
