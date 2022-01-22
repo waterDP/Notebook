8b924ae1e7229f5935c3399117e6e574297a2a08
li.haoran <li.haoran@99cloud.net>
2022-01-22 15:04:59 +0800

    .

--- JavaScript/flexible.js
+++ JavaScript/flexible.js
@@ -5,14 +5,14 @@
  * @FilePath: \notebook\JavaScript\flexible.js
  */
 // 首先是一个立即执行函数，执行时传入的参数是window和document
-(function flexible (window, document) {
+(function flexible(window, document) {
   let docEl = document.documentElement // 返回文档的root元素
-  let dpr = window.devicePixelRatio || 1 
+  let dpr = window.devicePixelRatio || 1
   // 获取设备的dpr，即当前设置下物理像素与虚拟像素的比值
 
   // 调整body标签的fontSize，fontSize = (12 * dpr) + 'px'
   // 设置默认字体大小，默认的字体大小继承自body
-  function setBodyFontSize () {
+  function setBodyFontSize() {
     if (document.body) {
       document.body.style.fontSize = (12 * dpr) + 'px'
     } else {
@@ -23,23 +23,21 @@
 
   // set 1rem = viewWidth / 10
   // 设置root元素的fontSize = 其clientWidth / 10 + ‘px’
-  function setRemUnit () {
+  function setRemUnit() {
     let rem = docEl.clientWidth / 10
     docEl.style.fontSize = rem + 'px'
   }
 
   setRemUnit()
-
-
-    // 当我们页面尺寸大小发生变化的时候，要重新设置下rem 的大小
-    window.addEventListener('resize', setRemUnit)
-        // pageshow 是我们重新加载页面触发的事件
-    window.addEventListener('pageshow', function(e) {
-        // e.persisted 返回的是true 就是说如果这个页面是从缓存取过来的页面，也需要从新计算一下rem 的大小
-        if (e.persisted) {
-            setRemUnit()
-        }
-    })
+  // 当我们页面尺寸大小发生变化的时候，要重新设置下rem 的大小
+  window.addEventListener('resize', setRemUnit)
+  // pageshow 是我们重新加载页面触发的事件
+  window.addEventListener('pageshow', function (e) {
+    // e.persisted 返回的是true 就是说如果这个页面是从缓存取过来的页面，也需要从新计算一下rem 的大小
+    if (e.persisted) {
+      setRemUnit()
+    }
+  })
 
   // 检测0.5px的支持，支持则root元素的class中有hairlines
   if (dpr >= 2) {
