fbd7ccef20483ca5189edb431f366c80d2cac995
li.haoran <li.haoran@99cloud.net>
2022-01-27 19:05:32 +0800

    .

--- 前端工程化/Webpack/原理/loaders/banner-loader.js
+++ 前端工程化/Webpack/原理/loaders/banner-loader.js
@@ -1,3 +1,9 @@
+/*
+ * @Author: water.li
+ * @Date: 2020-03-15 21:32:36
+ * @Description: 
+ * @FilePath: \notebook\前端工程化\Webpack\原理\loaders\banner-loader.js
+ */
 const loaderUtils = require('loader-utils')
 const validateOptions = require('schema-utils')
 const fs = require('fs')
@@ -21,7 +27,7 @@ function loader(source, sourceMap, ast) {
   if (options.filename) {
     this.addDependency(options.filename) // 添加文件依赖
     fs.readFile(options.filename, 'utf8', (err, data) => {
-      cb(err, `/**${data}**/${source}`, sourceMap, ast)
+      cb(err, `/**${data}**/${source}`, sourceMap, astoo )
     })
   } else {
     cb(null, `/**${options.text}**/${source}`, sourceMap, ast)
