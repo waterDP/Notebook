bccbf34910d6b89317863b50fd34dc53f6883de4
li.haoran <li.haoran@99cloud.net>
2022-02-25 23:21:17 +0800

    .

--- 微前端原理/index.js
+++ 微前端原理/index.js
@@ -4,9 +4,14 @@
  * @Description: 
  * @FilePath: \notebook\微前端原理\index.js
  */
-import handleRouter from "./handleRouter"
 import listenRouter from "./listenRouter"
 
+/**
+ * 1.监听路由变化
+ * 2.匹配子路由
+ * 3.加载子应用 
+ * 4.渲染子应用
+ */
 
 let _apps = []
 
