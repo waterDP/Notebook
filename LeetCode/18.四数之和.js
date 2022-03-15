c4c3289e7535b768ed133aa4bed13e5888b13a95
li.haoran <li.haoran@99cloud.net>
2022-03-15 22:05:30 +0800

    .

---
+++ LeetCode/18.四数之和.js
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode.cn id=18 lang=javascript
+ *
+ * [18] 四数之和
+ *
+ * https://leetcode-cn.com/problems/4sum/description/
+ *
+ * algorithms
+ * Medium (39.39%)
+ * Likes:    1153
+ * Dislikes: 0
+ * Total Accepted:    278.2K
+ * Total Submissions: 706.5K
+ * Testcase Example:  '[1,0,-1,0,-2,2]\n0'
+ *
+ * 给你一个由 n 个整数组成的数组 nums ，和一个目标值 target 。请你找出并返回满足下述全部条件且不重复的四元组 [nums[a],
+ * nums[b], nums[c], nums[d]] （若两个四元组元素一一对应，则认为两个四元组重复）：
+ * 
+ * 
+ * 0 <= a, b, c, d < n
+ * a、b、c 和 d 互不相同
+ * nums[a] + nums[b] + nums[c] + nums[d] == target
+ * 
+ * 
+ * 你可以按 任意顺序 返回答案 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [1,0,-1,0,-2,2], target = 0
+ * 输出：[[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = [2,2,2,2,2], target = 8
+ * 输出：[[2,2,2,2]]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 1 <= nums.length <= 200
+ * -10^9 <= nums[i] <= 10^9
+ * -10^9 <= target <= 10^9
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function(nums, target) {
+
+};
+// @lc code=end
+
