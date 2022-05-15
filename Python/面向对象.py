1068e4d6da3a239d76baa75c5db692d9383ec998
li.haoran <li.haoran@99cloud.net>
2022-05-15 23:05:27 +0800

    .

--- Python/面向对象.py
+++ Python/面向对象.py
@@ -1,36 +1,38 @@
 from msilib.schema import Property
 
+
 class Student:
-  # 固定此类的属性
-  __slots__ = ('name', 'age')
-  # __xxx 表示私有属性
-  def __init__(self, name, age):
-    self.__name = name
-    self.__age = age
+    # 固定此类的属性
+    __slots__ = ('name', 'age')
+    # __xxx 表示私有属性
 
-  # 静态方法
-  @staticmethod
-  def is_valid(a, b, c):
-    '''判断三条边长是否能构成三角形'''
-    return a + b > c and b + c > a and a + c > b
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
 
-  # 获取私有属性
-  @Property
-  def name(self):
-    return self.__name
-  
-  @name.setter
-  def name(self, name):
-    self.__name = name or '无名'
-  
-  def study(self, course_name):
-    print(f'学生{self.name}正在学习{course_name}')
+    # 静态方法
+    @staticmethod
+    def is_valid(a, b, c):
+        '''判断三条边长是否能构成三角形'''
+        return a + b > c and b + c > a and a + c > b
 
-  def play(self):
-    print('学生正在玩游戏')
+    # 获取私有属性
+    @Property
+    def name(self):
+        return self.__name
 
-  def __repr__(self) -> str:
-    return f'{self.name}: {self.__age}'
+    @name.setter
+    def name(self, name):
+        self.__name = name or '无名'
+
+    def study(self, course_name):
+        print(f'学生{self.name}正在学习{course_name}')
+
+    def play(self):
+        print('学生正在玩游戏')
+
+    def __repr__(self) -> str:
+        return f'{self.name}: {self.__age}'
 
 
 stu1 = Student('lhr', 12)
