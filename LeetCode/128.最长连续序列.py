ca607fbd07ae6dddc2bc9faeedb0640af05ea54f
li.haoran <li.haoran@99cloud.net>
2022-05-16 20:17:02 +0800

    .

--- LeetCode/128.最长连续序列.py
+++ LeetCode/128.最长连续序列.py
@@ -6,20 +6,20 @@
 
 # @lc code=start
 class Solution:
-    def longestConsecutive(self, nums: List[int]) -> int:
-        longest_streak = 0
-        num_set = set(nums)
+  def longestConsecutive(self, nums: List[int]) -> int:
+    longest_streak = 0
+    num_set = set(nums)
 
-        for num in num_set:
-            if num - 1 not in num_set:
-                current_num = num
-                current_streak = 1
+    for num in num_set:
+      if num - 1 not in num_set:
+        current_num = num
+        current_streak = 1
 
-                while current_num + 1 in num_set:
-                    current_num += 1
-                    current_streak += 1
+        while current_num + 1 in num_set:
+          current_num += 1
+          current_streak += 1
 
-                longest_streak = max(longest_streak, current_streak)
+        longest_streak = max(longest_streak, current_streak)
 
-        return longest_streak
-            # @lc code=end
+    return longest_streak
+    # @lc code=end
