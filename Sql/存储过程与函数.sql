d2efa2eafc6aa59aa8decae2a16440a3ac39549e
li.haoran <li.haoran@99cloud.net>
2022-05-23 22:18:09 +0800

    .

---
+++ Sql/存储过程与函数.sql
@@ -0,0 +1,103 @@
+#1 创建存储过程
+# 类型一 无参数无返回值
+# 举例1：创建存储过程select_all_data(), 相看emps表的所有数据
+delimiter $
+
+create procedure select_all_data()
+begin
+  select * from employees;
+end $
+
+delimiter ;
+
+# 存储过程的调用 
+call select_all_data();
+
+# 举例2：创建存储过程avg_employee_salary(), 返回所有员工的平均工资
+delimiter $
+create procedure avg_employee_salary()
+begin
+  select avg(salary) from employees;
+end $
+delimiter ;
+
+# 调用 
+call avg_employee_salary()
+
+# 类型二 带out
+# 举例 创建存储过程show_min_salary 查看emps表的最低薪资值。并将最低薪资通过OUT参数'ms'输出
+desc employees
+delimiter //
+create procedure show_min_salary(out ms double)
+begin
+  select min(salary) into ms
+  from emps;
+end //
+delimiter ;
+
+# 调用
+call show_min_salary(@ms)
+# 查看变量值
+select @ms;
+
+# 类型三 带in
+# 举例 创建存储过程show_someone_salary(),查看"emps"表的某个员工的薪资，并用in参数empname输入员工姓名
+delimiter //
+
+create procedure show_someone_salary(in empname varchar(20))
+begin
+  select salary from emps
+  where last_name = empname;
+end //
+
+delimiter ;
+
+# 调用方式一
+call show_someone_salary('Abel')
+# 调用方式二
+set @empname := 'Abel'
+call show_someone_salary(@empname)
+
+# 类型四 带in和out
+# 举例 创建存储过程show_someone_salary2(),查看emps表的某个员工的薪资
+# 并用in 参数empname输入员工姓名，用out参数empslary输出员工薪资
+delimiter //
+
+create procedure show_someone_salary(in empname varchar(2), out empsalary double)
+begin
+  select salary into empsalary
+  from emps
+  where last_name = empname;
+end //
+
+delimiter ;
+# 调用
+set @empname = 'Abel'
+call show_somone_slary2(@empname, @empsalary);
+
+select @empsalary
+
+# 类型五：带inout
+# 举例 创建存储过程show_mgr_name(),查询某个员工领导的姓名，并用inout参数empname输入员工姓名，输出领导的姓名
+delimiter $
+
+create procedure show_mgr_name(inout empname varchar(25))
+begin
+  select last_name into empname
+  from emps
+  where employee_id = (
+    select manager_id
+    from emps
+    where last_name = empname
+  );
+end $
+
+delimiter ;
+
+# 调用
+set @empname := 'Abel'
+call show_mgr_name(@empname)
+
+select @empname;
+
+# 存储函数
