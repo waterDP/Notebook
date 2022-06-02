536f3cef9c2d2bf467e63f43e0881d83ff902c62
water.li <289002199@qq.com>
2022-06-02 21:27:39 +0800

    .

--- Rust/Collection.rs
+++ Rust/Collection.rs
@@ -1,7 +1,7 @@
 fn main() {
   let v = vec![1,2,3];
   
-  let third: &i32 = $v[2];
+  let third: &i32 = &v[2];
   println!("The third element is {}", third)
   
   match v.get(2) {
@@ -9,7 +9,7 @@ fn main() {
     None => println("There is no third element")
   }
 
-  for i in $mut v {
+  for i in &mut v {
     *i += 50
   }
   for i in v {
