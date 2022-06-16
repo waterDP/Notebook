6cdd9187d6a9ac966197a40917b5ce160e291f86
water.li <289002199@qq.com>
2022-06-16 23:26:41 +0800

    .

--- Python/proxy_pool.py
+++ Python/proxy_pool.py
@@ -2,11 +2,12 @@
 Author: water.li
 Date: 2022-06-14 21:53:19
 LastEditors: water.li
-LastEditTime: 2022-06-14 22:04:11
-FilePath: \note\Python\proxy.py
+LastEditTime: 2022-06-16 21:38:37
+FilePath: \note\Python\proxy_pool.py
 '''
 
 from urllib import request
+from random import choice
 
 url = 'http://www.baidu.com/s?wd=ip'
 
@@ -16,8 +17,15 @@ headers = {
 
 req = request.Request(url=url, headers=headers)
 
-# 获取handler对象 
-handler = request.HTTPHandler()
+proxies_pools = [
+  {'http': '118.24.36.127:1716'},
+  {'http': '192.10.20.128:4331'},
+  {'http': '17.29.199.233:2234'}
+]
+proxies = choice(proxies_pools)
+
+# 获取handler对象
+handler = request.PorxyHandler(proxies = proxies)
 
 # 获取opener对象
 opener = request.build_opener(handler)
@@ -27,5 +35,6 @@ res = opener.open(req)
 
 content = res.read().decode('utf-8')
 
-print(content)
-
+# 保存
+with open('daili.html', 'w', encoding="utf-8") as fp:
+  fp.write(content)
