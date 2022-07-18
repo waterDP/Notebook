f720a541677a8ada8da3ba81700fb49e40b2b3d5
water.li <289002199@qq.com>
2022-07-18 23:27:34 +0800

    .

--- 设计模式/策略模式.ts
+++ 设计模式/策略模式.ts
@@ -1,3 +1,9 @@
+/*
+ * @Author: water.li
+ * @Date: 2022-04-16 20:38:06
+ * @Description: 
+ * @FilePath: \note\设计模式\策略模式.ts
+ */
 /**
  * @description Context: 用来操作策略的上下文环境
  * @description Strategic: 策略的抽象
@@ -36,3 +42,30 @@ class TransStrategy {
 
 let tranStrategy = new TransStrategy()
 console.log(tranStrategy.calc(5)) // 10
+
+// 虚拟代理 把一些开销很大的对象，延迟到真正需要它的时候再去创建它
+function myImageFun() {
+  let imgNode = document.createElement('img')
+  document.body.append(imgNode)
+  return {
+    setSrc(src) {
+      imgNode.src = src
+    }
+  }
+}
+let imgSlot = myImageFun()
+
+let proxyImgFun = () => {
+  let img = new Image()
+  img.onload = function() {
+    imgSlot.setSrc((<any>this).src)
+  }
+  return {
+    setSrc(src) {
+      imgSlot.setSrc('小的默认图片')
+      img.src = src
+    }
+  }
+}
+let proxyImg = proxyImgFun()
+proxyImg.setSrc('http://sehnfgiur.mvieq/veni/di3uq.43940.jpg')
