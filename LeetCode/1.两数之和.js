09fc2e15fbc3fe209a9b63700d7b37d86b0a063d
water.li <289002199@qq.com>
2022-09-25 16:46:51 +0800

    ./

---
+++ LeetCode/1.两数之和.js
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=javascript
+ *
+ * [1] 两数之和
+ *
+ * https://leetcode.cn/problems/two-sum/description/
+ *
+ * algorithms
+ * Easy (52.49%)
+ * Likes:    14520
+ * Dislikes: 0
+ * Total Accepted:    3.4M
+ * Total Submissions: 6.5M
+ * Testcase Example:  '[2,7,11,15]\n9'
+ *
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ * 
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ * 
+ * 你可以按任意顺序返回答案。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = [3,2,4], target = 6
+ * 输出：[1,2]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：nums = [3,3], target = 6
+ * 输出：[0,1]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 2 <= nums.length <= 10^4
+ * -10^9 <= nums[i] <= 10^9
+ * -10^9 <= target <= 10^9
+ * 只会存在一个有效答案
+ * 
+ * 
+ * 进阶：你可以想出一个时间复杂度小于 O(n^2) 的算法吗？
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  let map = {}
+  for (let i = 0; i < nums.length; i++) {
+    let temp = target - nums[i]
+    if (typeof map[temp] === 'number') {
+      return [map[temp], i]
+    }
+    map[nums[i]] = i
+  }
+  return [-1, -1]
+};
+// @lc code=end
+
