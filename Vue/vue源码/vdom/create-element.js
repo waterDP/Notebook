a6eab4e2ba6a8e7f75fb5eca6f93c948e4ba0508
water.li <289002199@qq.com>
2022-10-05 19:45:56 +0800

    .

--- Vue/vue源码/vdom/create-element.js
+++ Vue/vue源码/vdom/create-element.js
@@ -1,8 +1,14 @@
+/*
+ * @Author: water.li
+ * @Date: 2022-04-16 20:38:06
+ * @Description: 
+ * @FilePath: \note\Vue\vue源码\vdom\create-element.js
+ */
 import {createComponent} from './create-component'
 import {vnode} from './vnode'
 import {isReservedTag} from '../util/index'
 
-export function createElement(vm, tag, data={}, ...children) {
+export function createElement(vm, tag,  ={}, ...children) {
   let key = data.key
   delete data.key
   if (isReservedTag(tag)) {
