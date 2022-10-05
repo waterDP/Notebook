eab79099972e535fd10973d8b4d33bea0a6c3269
water.li <289002199@qq.com>
2022-10-05 20:53:45 +0800

    ..

--- Vue/vue源码/observer/scheduler.js
+++ Vue/vue源码/observer/scheduler.js
@@ -1,3 +1,9 @@
+/*
+ * @Author: water.li
+ * @Date: 2022-04-16 20:38:06
+ * @Description: 
+ * @FilePath: \note\Vue\vue源码\observer\scheduler.js
+ */
 import {nextTick} from '../util/next-tick'
 
 let queue = []
@@ -11,7 +17,7 @@ function flushSchedulerQueue() {
 
 export function queueWatcher(watcher) {
   const id = watcher.id
-  if (has[id] === undefined) {
+  if (!has[id]) {
     queue.push(watcher)
     has[id] = true
     nextTick(flushSchedulerQueue)
