e3c8712804fb6b633cebbec44bf81c2753a201c2
water.li <289002199@qq.com>
2022-10-06 13:16:43 +0800

    .

--- Vue/vue源码/global-api/mixin.js
+++ Vue/vue源码/global-api/mixin.js
@@ -1,7 +1,14 @@
+/*
+ * @Author: water.li
+ * @Date: 2022-04-16 20:38:06
+ * @Description: 
+ * @FilePath: \note\Vue\vue源码\global-api\mixin.js
+ */
 import {mergeOptions} from '../util/index'
 
 export function initMixin(Vue) {
   Vue.mixin = function (mixin) {
     this.options = mergeOptions(this.options, mixin)
+    return this
   }
 }
