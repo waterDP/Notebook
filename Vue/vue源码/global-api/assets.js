6f0c8c8d000467c4a62296bfcd86687d2a247684
water.li <289002199@qq.com>
2022-10-07 21:26:27 +0800

    .

--- Vue/vue源码/global-api/assets.js
+++ Vue/vue源码/global-api/assets.js
@@ -1,19 +1,37 @@
-import {ASSETS_TYPE} from './constant'
+/*
+* @Author: water.li
+* @Date: 2022-04-16 20:38:06
+* @Description: 
+ * @FilePath: \note\Vue\vue源码\global-api\assets.js
+*/
+
+import { isFunction } from "../util"
+
+const ASSETS_TYPE = ['component', 'directive', 'filter']
 
 export function initAssetRegisters(Vue) {
   ASSETS_TYPE.forEach(type => {
-    Vue[type] = function(id, definition) {
-      if (type === 'component') {
-        // 注册全局组件
-        // 使用extend方法，将对象变成构造函数
-        definition = this.options._base.extend(definition)
-      } else if (type === 'filter') {
-
-      } else if (type === 'directive') {
-
+    /**
+     * @param {string} id
+     * @param {Function|Object} definition
+     * @return {Function|Object|Void}
+     */
+    Vue[type] = function (id, definition) {
+      if (!definition) {
+        return this.options[type+'s'](id)
       }
-
-      this.options[type + 's'][id] = definition
+      if (type === 'component') {
+        definition.name = definition.name || id
+        definition = this.options._base.extend(definition)
+      }
+      if (type === 'directive' && isFunction(definition)) {
+        definition = {
+          bind: definition,
+          update: defniition
+        }
+      }
+      this.options[type+'s'][id] = definition
+      return definition
     }
   })
 }
--- Vue/vue源码/global-api/constant.js
+++
@@ -1,5 +0,0 @@
-export const ASSETS_TYPE = [
-  'component',
-  'directive',
-  'filter'
-]
