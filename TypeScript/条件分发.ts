30581bc4461a78e5bddac3796ea9eac55e5d41d5
water.li <289002199@qq.com>
2023-01-26 13:58:51 +0800

    .

--- TypeScript/条件分发.ts
+++ TypeScript/条件分发.ts
@@ -8,98 +8,117 @@
 // 条件类型 extends 约束 经常和条件一起使用
 // 条件类型的格式可以看成是三元表达式
 
-type ResStatusMessage<T> = T extends 200 | 201 | 204 | 206 ? 'success' : 'fail'
+type ResStatusMessage<T> = T extends 200 | 201 | 204 | 206 ? "success" : "fail";
 
-type IMessage = ResStatusMessage<200>
+type IMessage = ResStatusMessage<200>;
 // 对于联合类型而言子类型是其中的任何一个
 // 交叉后的结果就是交叉前的某个类型的子类型 Exclude Extract
 
-type Conditional<T, U> = T extends U ? true : false
+type Conditional<T, U> = T extends U ? true : false;
 
-type FormatReturnValue<T> =
-  T extends string
-  ? string :
-  T extends number
-  ? number : never
+type FormatReturnValue<T> = T extends string
+  ? string
+  : T extends number
+  ? number
+  : never;
 function sum<T extends string | number>(a: T, b: T): FormatReturnValue<T> {
-  return a + (b as any)
+  return a + (b as any);
 }
-let r = sum(1, 2)
+let r = sum(1, 2);
 
 /**
  * ^ extends 父子关系 （类型等级）
- * never和字面量的关系 
+ * never和字面量的关系
  * 字面量类型和基础类型的关系
  * 基础类型和包装类型
  */
 
-// * never和字面量的关系  
-type T1 = never extends '123' ? true : false // 子类型可以赋予给父类型
+// * never和字面量的关系
+// never是最底端的类型 即never可以是任何类型的子类型
+type T1 = never extends "123" ? true : false; // 子类型可以赋予给父类型
 let a: number = (function (): never {
-  throw new Error()
-})()
+  throw new Error();
+})();
 
 // * 字面量类型和基础类型的关系 字面量类型肯定是基础类型的子类型
-type T2 = 123 extends number ? true : false
-type T3 = 'abc' extends string ? true : false
+type T2 = 123 extends number ? true : false;
+type T3 = "abc" extends string ? true : false;
 
 // * 基础类型和包装类型 基础类型是包装类型的子类型
-type T4 = string extends String ? true : false
-type T5 = String extends Object ? true : false
+type T4 = string extends String ? true : false;
+type T5 = String extends Object ? true : false;
 
 // * any 所有类型都是any和unknown的子类型 any == unknown 顶端类型
-type T6 = Object extends unknown ? true : false
+type T6 = Object extends unknown ? true : false;
+
+interface Fish {
+  name: "鱼";
+}
+
+interface Water {
+  type: "水";
+}
+
+interface Bird {
+  name: "鸟";
+}
+
+interface Sky {
+  type: "天空";
+}
+
+type SelectType<T> = T extends Fish ? Water : Sky;
+
+type F7 = SelectType<Bird | Fish>;
+type F8 = Bird | Fish extends Fish ? Water : Sky;
 
 /**
  * & 分发机制
  * 1.通过泛型传入的方式来比较的时候会出现分发
- * 2.类型需要是联合类型 | 
- * 3.类型需要完全的裸露出来 
- * 
+ * 2.类型需要是联合类型 |
+ * 3.类型需要完全的裸露出来
+ *
  * never做比较的暑假 也会有分发问题
  *   any 默认分发
  *   never 默认只有通过泛型传递的时候会返回never
  */
-type NoDistrubute<T> = T & {}
-type UnionAssets<T, U> = NoDistrubute<T> extends U ? true : false
+type NoDistribute<T> = T & {};
+type UnionAssets<T, U> = NoDistribute<T> extends U ? true : false;
 
-type T8 = UnionAssets<1 | 2, 1 | 2 | 3>
-type T9 = UnionAssets<1 | 2 | 3, 1 | 2>
+type T8 = UnionAssets<1 | 2, 1 | 2 | 3>;
+type T9 = UnionAssets<1 | 2 | 3, 1 | 2>;
 
 // 如果判断两个类型完全相等
-type IsEqual<T, U, Success, Fail> = NoDistrubute<T> extends U
-  ? (NoDistrubute<U> extends T
+type IsEqual<T, U, Success, Fail> = NoDistribute<T> extends U
+  ? NoDistribute<U> extends T
     ? Success
-    : Fail)
-  : Fail
+    : Fail
+  : Fail;
 
-type T10 = IsEqual<1 | 2, 1 | 2, true, false>
+type T10 = IsEqual<1 | 2, 1 | 2, true, false>;
 
 // never   作比较的时候，也会有分发的问题
 // - any   默认分发
 // - never 默认只通过泛型传递的时候会返回never
-type isNever<T> = NoDistrubute<T> extends never ? true : false
-type T11 = isNever<never>
-
-
+type isNever<T> = NoDistribute<T> extends never ? true : false;
+type T11 = isNever<never>;
 
 // ^ 内置的条件类型 Extract Exclude NonNullable 集合操作
 
-// 获取两个联合类型的交集
-type Extract<T, U> = T extends U ? T : never  // 分发
-type E1 = Extract<string | number, string | number | boolean>
+// & 获取两个联合类型的交集 Extract
+type Extract<T, U> = T extends U ? T : never; // 分发
+type E1 = Extract<string | number, string | number | boolean>;
 
+// & 差集 Exclude
+type Exclude<T, U> = T extends U ? never : T;
+type E2 = Exclude<string | number | boolean, string | number | symbol>;
 
-// 差集
-type Exclude<T, U> = T extends U ? never : T
-type E2 = Exclude<string | number | boolean, string | number | symbol>
+// 补集 差集+子类的关系
+type Complement<T, U extends T> = T extends U ? never : T;
+type E3 = Complement<string | number | boolean, string>;
 
-// 补集 差集+子类的关系 
-type Complement<T, U extends T> = T extends U ? never : T
-type E3 = Complement<string | number | boolean, string>
+let ele = document.getElementById("root");
+type NonNullable<T> = T extends null | undefined ? never : T;
+type EleElement = NonNullable<typeof ele>;
 
-let ele = document.getElementById('root')
-type NonNullable<T> = T extends null | undefined ? never : T
-type EleElement = NonNullable<typeof ele>
-
-export { }
+export {};
--- TypeScript/自定义类型.ts
+++ TypeScript/自定义类型.ts
@@ -1,14 +1,13 @@
 /*
  * @Author: water.li
  * @Date: 2023-01-12 21:38:27
- * @Description: 
+ * @Description:
  * @FilePath: \Notebook\TypeScript\自定义类型.ts
  */
 
+export {};
 
-export { }
-
-// * Exclude Extract 集合的操作
+// * Exclude(排除) Extract(提取) 集合的操作
 // * Pick Omit 对对象结构的操作
 // * Partial Required Readonly 起修饰作用的
 // * ReturnType Paramaters InstanceType ... (基于infer)
@@ -16,90 +15,93 @@ export { }
 interface Person {
   name: string;
   age: number;
-  company: string
+  company: string;
 }
 
 type Compute<T extends object> = {
-  [K in keyof T]: T[K]
-}
+  [K in keyof T]: T[K];
+};
 
-// ? 让部分属性变为可选的 
+// ? 让部分属性变为可选的
 
 // 先将name属性挑出来变为可选的 & 除了name属性的
-type PartialPropsOptional<T, K extends keyof T> = Partial<Pick<T, K>> & Omit<T, K>
+type PartialPropsOptional<T, K extends keyof T> = Partial<Pick<T, K>> &
+  Omit<T, K>;
 
-type T1 = PartialPropsOptional<Person, 'name'>
-type CT1 = Compute<T1>
+type T1 = PartialPropsOptional<Person, "name">;
+type CT1 = Compute<T1>;
 
 // ? 根据值的类型来选择key
-type IsEqual<T, U, Success, Fail> = [T] extends [U] ? [U] extends [T] ? Success : Fail : Fail
+type IsEqual<T, U, Success, Fail> = [T] extends [U]
+  ? [U] extends [T]
+    ? Success
+    : Fail
+  : Fail;
 type ExtractKeysByValueType<T, U> = {
-  [K in keyof T]: IsEqual<T[K], U, K, never>
-}[keyof T] // 找到需要的属性
+  [K in keyof T]: IsEqual<T[K], U, K, never>;
+}[keyof T]; // 找到需要的属性
 
-type T2 = ExtractKeysByValueType<Person, string>
+type T2 = ExtractKeysByValueType<Person, string>;
 
-type PickKeysByValue<T, U> = Pick<T, ExtractKeysByValueType<T, U>>
+type PickKeysByValue<T, U> = Pick<T, ExtractKeysByValueType<T, U>>;
 
-type T3 = PickKeysByValue<Person, string>
+type T3 = PickKeysByValue<Person, string>;
 
 type OmitKeysByValueType<T, U> = {
-  [K in keyof T]: IsEqual<T[K], U, never, K>
-}[keyof T]
+  [K in keyof T]: IsEqual<T[K], U, never, K>;
+}[keyof T];
 
-type OmitKeysByValue<T, U> = Pick<T, OmitKeysByValueType<T, U>>
+type OmitKeysByValue<T, U> = Pick<T, OmitKeysByValueType<T, U>>;
 
-type T4 = OmitKeysByValue<Person, string>
+type T4 = OmitKeysByValue<Person, string>;
 
 // ! as 重映射
 type PickKeysByValueType<T extends object, U> = {
-  [K in keyof T as T[K] extends U ? K : never]: T[K]
-}
-type T5 = PickKeysByValueType<Person, string>
+  [K in keyof T as T[K] extends U ? K : never]: T[K];
+};
+type T5 = PickKeysByValueType<Person, string>;
 
 type A = {
   name: string;
   age: number;
   address: string;
-}
+};
 
 type B = {
   name: string;
   male: boolean;
   address: number;
-}
+};
 
 // & 求交集 {name: string, address: number}
 
 type ObjectInter<T extends object, U extends object> = Pick<
   U,
   Extract<keyof T, keyof U>
->
+>;
 
-type xx1 = ObjectInter<A, B>
+type xx1 = ObjectInter<A, B>;
 
-// & 求差集 B - A 
+// & 求差集 B - A
 type ObjectDiff<T extends object, U extends object> = Omit<
   U,
   Extract<keyof T, keyof U>
->
+>;
 type xx2 = ObjectDiff<A, B>;
-
 
 type A1 = {
   name: string;
-  age: number
+  age: number;
   address: string;
-}
+};
 
 type B1 = {
   name: string;
   address: string;
-}
+};
 // & 求补集  求B1在A1中的补集
 type ObjectComp<T extends object, U extends T> = Omit<
   U,
   Extract<keyof T, keyof U>
->
+>;
 type xx3 = ObjectComp<B1, A1>;
-
