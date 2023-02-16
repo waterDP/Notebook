0656b97fc07cd3dd45bd74e51c7b0e377b491047
water.li <289002199@qq.com>
2023-02-16 22:56:19 +0800

    rollup plugin

--- 前端工程化/rollup/base/rollup.config.js
+++ 前端工程化/rollup/base/rollup.config.js
@@ -1,40 +1,40 @@
 /*
  * @Author: water.li
  * @Date: 2023-02-04 11:18:13
- * @Description: 
- * @FilePath: \Notebook\前端工程化\rollup\rollup.config.js
+ * @Description:
+ * @FilePath: \Notebook\前端工程化\rollup\base\rollup.config.js
  */
-import babel from "@rollup/plugin-babel"
-import nodeResolve from "@rollup/plugin-node-resolve"
-import commonjs from "@rollup/plugin-commonjs"
-// import typescript from "@rollup/plugin-typescript"
-import { terser } from "rollup-plugin-terser"
-import serve from "rollup-plugin-serve"
+import babel from "@rollup/plugin-babel";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import { terser } from "rollup-plugin-terser";
+import serve from "rollup-plugin-serve";
 
 export default {
-  input: 'src/main.js',
+  input: "src/main.js",
   output: {
-    file: 'dist/bundle.cjs.js', // 输出的文件路径和文件名
-    format: 'iife', // 五种输出的格式 amd/es/iife/umd/cjs
-    name: 'bundleName', // 当format的格式为iife和umd的时候必须提供变量名
+    file: "dist/bundle.cjs.js", // 输出的文件路径和文件名
+    format: "iife", // 五种输出的格式 amd/es/iife/umd/cjs
+    name: "bundleName", // 当format的格式为iife和umd的时候必须提供变量名
     globals: {
-      lodash: '_',
-      jquery: '$'
-    }
+      lodash: "_",
+      jquery: "$",
+    },
   },
-  external: ['lodash', 'jquery'],
+  external: ["lodash", "jquery"], // 外部的
   plugins: [
     babel({
-      exclude: /node_modules/
+      exclude: /node_modules/,
     }),
     nodeResolve(), // 作用是可以加载node_modules里有的模块
     commonjs(), // 可以支持commonjs语法
-   // typescript(), // 支持ts
+    typescript(), // 支持ts
     terser(), // 压缩js
     serve({
       open: true,
       port: 8080,
-      contentBase: './dist'
-    })
-  ]
-}
+      contentBase: "./dist",
+    }),
+  ],
+};
