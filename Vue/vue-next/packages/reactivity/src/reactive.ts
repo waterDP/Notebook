c33b5102b80f4b36eb924731d55b2c3e19bdf42e
water.li <289002199@qq.com>
2023-03-10 22:13:09 +0800

    111

--- Vue/vue-next/packages/reactivity/src/reactive.ts
+++ Vue/vue-next/packages/reactivity/src/reactive.ts
@@ -1,70 +1,69 @@
 /*
  * @Author: water.li
  * @Date: 2022-04-08 21:36:15
- * @Description: 
+ * @Description:
  * @FilePath: \note\Vue\vue-next\packages\reactivity\src\reactive.ts
  */
 
-import { isObject } from "@vue/shared"
-import { track, trigger } from "./effect"
+import { isObject } from "@vue/shared";
+import { track, trigger } from "./effect";
 
 const enum ReactiveFlags {
-  IS_REACTIVE = '__v_isReactive'
+  IS_REACTIVE = "__v_isReactive",
 }
 
 const mutableHandlers: ProxyHandler<Record<any, any>> = {
   get(target, key, recevier) {
     if (key === ReactiveFlags.IS_REACTIVE) {
-      return true
+      return true;
     }
-    track(target, key)
-    const  res = Reflect.get(target, key, recevier)
+    track(target, key);
+    const res = Reflect.get(target, key, recevier);
 
     // 这里取值了，可以收集他在哪个effect中
-    return res
+    return res;
   },
   set(target, key, value, recevier) {
-    let oldValue = (<any>target)[key]
-    const res = Reflect.set(target, key, value, recevier)
+    let oldValue = (<any>target)[key];
+    const res = Reflect.set(target, key, value, recevier);
 
-    if (oldValue !== value ) {
+    if (oldValue !== value) {
       // 如果改变值了，可以在这里触发effect更新
-      trigger(target, key) // 找属性对应的effect，让他重新执行
+      trigger(target, key); // 找属性对应的effect，让他重新执行
     }
-    return res
-  }
-}
+    return res;
+  },
+};
 
-const reactiveMap = new WeakMap() // 缓存已代理的对象，防止重复代理
+const reactiveMap = new WeakMap(); // 缓存已代理的对象，防止重复代理
 
 function createReactiveObject(target: object, isReadonly: boolean) {
-
   // 先默认认为这个target已经是代理过的属性了
   if ((<any>target)[ReactiveFlags.IS_REACTIVE]) {
-    return target
+    return target;
   }
 
   if (!isObject(target)) {
-    return target 
+    return target;
   }
 
-  const exisitingProxy = reactiveMap.get(target)
+  const exisitingProxy = reactiveMap.get(target);
   if (exisitingProxy) {
     // 如果已经代理了，就直接从缓存中返回
-    return exisitingProxy
+    return exisitingProxy;
   }
 
-  const proxy = new Proxy(target, mutableHandlers)
-  reactiveMap.set(target, proxy) // 将原对象和生成的代理对象，做一个映射表 防止重复代理
-  return proxy
+  const proxy = new Proxy(target, mutableHandlers); // ! 代理
+  reactiveMap.set(target, proxy); // 将原对象和生成的代理对象，做一个映射表 防止重复代理
+  return proxy;
 }
 
 export function reactive(target: object) {
-  return createReactiveObject(target, false)
+  return createReactiveObject(target, false);
 }
 
-export function toReactive(value) { 
-  return isObject(value) ? reactive(value) : value
+export function toReactive(value) {
+  return isObject(value) ? reactive(value) : value;
 }
 
 export function readonly(target: object) {
--- Vue/vue-next/rollup.config.js
+++ Vue/vue-next/rollup.config.js
@@ -1,66 +1,64 @@
 /*
  * @Author: water.li
  * @Date: 2022-04-08 00:30:15
- * @Description: 
+ * @Description:
  * @FilePath: \notebook\Vue\vue-next\rollup.config.js
  */
-import path from 'path'
-import ts from 'rollup-plugin-typescript2'
-import json from '@rollup/plugin-json'
-import {nodeResolve} from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
+import path from "path";
+import ts from "rollup-plugin-typescript2";
+import json from "@rollup/plugin-json";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 
-const packageFormats = process.env.FORMATS && process.env.FORMATS.split(',')
-const sourcemap = process.env.SOURCE_MAP
-const target = process.env.TARGET
+const packageFormats = process.env.FORMATS && process.env.FORMATS.split(",");
+const sourcemap = process.env.SOURCE_MAP;
+const target = process.env.TARGET;
 
-const packagesDir = path.resolve(__dirname, 'packages')
-const packageDir = path.resolve(packagesDir, target)
+// ^ 需要根据target 找到要打包的目录
+const packagesDir = path.resolve(__dirname, "packages");
+const packageDir = path.resolve(packagesDir, target);
 
-const resolve = p => path.resolve(packageDir, p)
-const name = path.basename(packageDir)
+const resolve = (p) => path.resolve(packageDir, p); // 以打包的目录解析文件
+const name = path.basename(packageDir);
 
-const pkg =  require(resolve('package.json'))
+const pkg = require(resolve("package.json"));
 
-console.log(name)
+console.log(name);
 
 const outputConfig = {
   "esm-bundler": {
     file: resolve(`dist/${name}.esm-bundle,js`),
-    format: 'es'
+    format: "es",
   },
-  "cjs": {
+  cjs: {
     file: resolve(`dist/${name}.cjs.js`),
-    format: 'cjs'
+    format: "cjs",
   },
-  "global": {
+  global: {
     file: resolve(`dist/${name}.global.js`),
-    format: 'iife'
-  }
-}
+    format: "iife",
+  },
+};
 
-const packageConfigs = packageFormats || pkg.buildOptions.formats
+const packageConfigs = packageFormats || pkg.buildOptions.formats;
 
 function createConfig(format, output) {
-  output.sourcemap = sourcemap // 添加sourcemap
-  output.exports = 'named'
-  let external = []
-  if (format === 'global') {
-    output.name = pkg.buildOptions.name
+  output.sourcemap = sourcemap; // 添加sourcemap
+  output.exports = "named";
+  let external = [];
+  if (format === "global") {
+    output.name = pkg.buildOptions.name;
   } else {
-    external = [...Object.keys(pkg.dependencies)]
+    external = [...Object.keys(pkg.dependencies)];
   }
   return {
     input: resolve(`src/index.ts`),
     output,
     external,
-    plugins: [
-      json(),
-      ts(),
-      commonjs(),
-      nodeResolve()
-    ]
-  }
+    plugins: [json(), ts(), commonjs(), nodeResolve()],
+  };
 }
 
-export default packageConfigs.map(format => createConfig(format, outputConfig[format]))
+export default packageConfigs.map((format) =>
+  createConfig(format, outputConfig[format])
+);
