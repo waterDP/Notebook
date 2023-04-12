977c00000ac7da989d3e6c20dffbcea4f9d473a2
water.li <289002199@qq.com>
2023-04-12 22:49:14 +0800

    webpack loader

--- 前端工程化/Webpack/原理/loaders/babel-loader.js
+++ 前端工程化/Webpack/原理/loaders/babel-loader.js
@@ -1,9 +1,18 @@
-const babel = require('@babel/core')
-const loaderUtils = require('loader-utils')
-function loader(inputSource) { // this loader的上下文
-  let options = loaderUtils.getOptions(this)
-  let {code, map, ast} = babel.transform(inputSource, options)
-  return this.callback(null, code, map, ast)
+const babel = require("@babel/core");
+const loaderUtils = require("loader-utils");
+
+/**
+ *
+ * @param {*} content 资源文件的内容，对于起始loader 只有这一个参数
+ * @param {*} map 前面loader生成的source map 可以传递给后方的loader共享
+ * @param {*} meta 其它需要 传给后方loader的共享信息，可自定义
+ * @returns
+ */
+function loader(content, map, meta) {
+  // this loader的上下文
+  let options = loaderUtils.getOptions(this);
+  let { code, map, ast } = babel.transform(content, options);
+  return this.callback(null, code, map, ast);
 }
 
-module.exports = loader
+module.exports = loader;
