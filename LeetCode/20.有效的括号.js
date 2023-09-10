4535915de4c1fa5b1157c0d319a337c47f053f56
water.li <289002199@qq.com>
2023-09-10 14:04:33 +0800

    .

--- LeetCode/20.有效的括号.js
+++ LeetCode/20.有效的括号.js
@@ -1,8 +1,8 @@
 /*
  * @Author: water.li
  * @Date: 2021-09-29 22:34:31
- * @Description: 
- * @FilePath: \notebook\LeetCode\20.有效的括号.js
+ * @Description:
+ * @FilePath: \Notebook\LeetCode\20.有效的括号.js
  */
 /*
  * @lc app=leetcode.cn id=20 lang=javascript
@@ -15,30 +15,23 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-  let stack = []
-  let len = s.length
-  if (len%2) return false
-  let lefts = '{[('
+var isValid = function (s) {
+  let stack = [];
+  let len = s.length;
+  if (len % 2) return false;
+  const lefts = "{[(";
+  const matchs = ["()", "[]", "{}"];
   for (let i = 0; i < len; i++) {
-    const item = s[i]
+    const item = s[i];
     if (lefts.includes(item)) {
-      stack.push(item)
+      stack.push(item);
     } else {
-      const top = stack.pop()
-      switch(item) {
-        case ')': 
-          if (top !== '(') return false
-        case ']':
-          if (top !== '[') return false
-        case  '}':
-          if (top !== '{') return false
-        default: 
-          return false
+      const top = stack.pop();
+      if (!matchs.includes(top + item)) {
+        return false;
       }
     }
   }
-  return !stack.length
-}
+  return !stack.length;
+};
 // @lc code=end
-
