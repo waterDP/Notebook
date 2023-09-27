340a2a20a40fa722c3b0a853d7ca1216f87e4412
water.li <289002199@qq.com>
2023-09-27 09:28:26 +0800

    rxjs-event

--- RxJS/source/internal/observable/fromEvent.js
+++ RxJS/source/internal/observable/fromEvent.js
@@ -11,5 +11,10 @@ export function fromEvent(target, eventName) {
   return new Observable((subscriber) => {
     const handler = (...args) => subscriber.next(...args);
     target.addEventListener(eventName, handler);
+
+    // _subscribe执行的结果可以返回一个取消订阅的函数
+    return () => {
+      target.removeEventListener(eventName, handler);
+    };
   });
 }
