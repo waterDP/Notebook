065073c9de48ed7ae452f5dddb232c22e5fc86ce
water.li <289002199@qq.com>
2024-04-06 18:49:19 +0800

    .

--- WebRTC/server.js
+++ WebRTC/server.js
@@ -1,16 +1,20 @@
 "use strict";
 
+const http = require("http");
 const https = require("https");
 const fs = require("fs");
+const express = require("express");
+const serveIndex = require("serve-index");
+
+const app = express();
+app.use(serveIndex("./www"));
+app.use(express.static("./www"));
 
 const options = {
   key: fs.readFileSync("./ssl_server.key"),
-  cert: fs.readFileSync("./ssl_server.cert"),
+  cert: fs.readFileSync("./ssl_server.crt"),
 };
 
-const app = https
-  .createServer(options, (req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("hello https\n");
-  })
-  .listen(443, "0.0.0.0");
+const httpServer = http.createServer(app).listen(80, "0.0.0.0");
+
+const httpsServer = https.createServer(options, app).listen(443, "0.0.0.0");
