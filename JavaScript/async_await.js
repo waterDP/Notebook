7ab921c1b82da7ef3728d435cb90f7971a6c38d3
water.li <289002199@qq.com>
2024-04-19 20:59:58 +0800

    .

--- JavaScript/async_await.js
+++ JavaScript/async_await.js
@@ -1,56 +1,55 @@
 // 示例
 function getData() {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     setTimeout(() => {
-      resolve('data')
-    }, 1000)
-  })
+      resolve("data");
+    }, 1000);
+  });
 }
 
 async function test() {
-  const data = await getData()
-  console.log('data', data)
-  const data2 = await getData()
-  console.log('data2', data)
-  return 'success'
+  const data = await getData();
+  console.log("data", data);
+  const data2 = await getData();
+  console.log("data2", data);
+  return "success";
 }
 // 这样一个函数 应该在1秒后在打印一个data 再过一秒打印一个data 最后打印success
-test().then(res => console.log(res))
+test().then((res) => console.log(res));
 
-
-// ? 思路 
+// ? 思路
 // 对于这个简单的案例来说，如果我们把它用generator函数表达，会是怎么样的呢
-function * testG() {
+function* testG() {
   // await编译成yield
-  const data = yield getData()
-  console.log('data', data) 
-  const data2 = yield getData()
-  console.log('data2', data)
-  return 'success'
+  const data = yield getData();
+  console.log("data", data);
+  const data2 = yield getData();
+  console.log("data2", data2);
+  return "success";
 }
-let gen = testG()
-let dataPromise = gen.next()
+let gen = testG();
+let dataPromise = gen.next();
 
 // 异步串行
-dataPromise.then(value1 => {
+dataPromise.then((value1) => {
   //data1的value被拿到了 继续调用next并且传递给data
-  let data2Promise = gen.next(value1)
+  let data2Promise = gen.next(value1);
   // console.log('data', data)
   // 此时就会打印出data
-  data2Promise.then(value2 => {
+  data2Promise.then((value2) => {
     // data2的value拿到了 继续调用next并且传递给value2
-    gen.next(value2)
+    gen.next(value2);
     // console.log('data2', data2)
     // 此时就会打印出data2
-  })
-})
+  });
+});
 
 function asyncToGenerator(generatorFunc) {
   // 返回的是一个新的函数
-  return function() {
+  return function () {
     // 先调用generator函数，生成迭代器
     // 对应 let gen = testG()
-    const gen = generatorFunc.apply(this, arguments)
+    const gen = generatorFunc.apply(this, arguments);
 
     // 返回一个promise因为外部是用.then的方法或者await的方法使用这个函数返回值的
     return new Promise((resolve, reject) => {
@@ -58,21 +57,21 @@ function asyncToGenerator(generatorFunc) {
       // key 有next 和 throw两种取值，分别对应了gen的next和throw方法
       // arg参数则是用来把promise resolve出来的值交给下一个 yield
       function step(key, arg) {
-        let generatorResult
+        let generatorResult;
         // 这个方法需要包装在try catch中
         // 如果报错了 就把promise给reject掉  外部通过.catch可以获取得到错误
         try {
-          generatorResult = gen[key](arg)
+          generatorResult = gen[key](arg);
         } catch (error) {
-          return reject(error)
+          return reject(error);
         }
         // gen.next() 得到的结果是一个{value, next}的结构
-        const {value, done} = generatorResult
+        const { value, done } = generatorResult;
 
         if (done) {
           // 如果已经完成了 就直接resolve这个promise
           // 这个done是在最后一次调用next后才会为true
-          return resolve(value)
+          return resolve(value);
         } else {
           // 除了最后结束的时候外，每次调用gen.next()
           // 其实是返回｛value:Promise, done: false｝的结构
@@ -86,25 +85,25 @@ function asyncToGenerator(generatorFunc) {
             // 并且只要done不是true的时候 就会递归的往下解开promise
             // 对应gen.next().value.then(value => {
             //    gen.next(value).value.then(value2 => {
-            //       gen.next() 
+            //       gen.next()
             //
-            //      // 此时done为true了 整个promise被resolve了 
+            //      // 此时done为true了 整个promise被resolve了
             //      // 最外部的test().then(res => console.log(res))的then就开始执行了
             //    })
             // })
-            function onResolve (val) {
-              step('next', value)
+            function onResolve(val) {
+              step("next", value);
             },
             // 如果promise被reject了，就再次进入step函数
             // 不同的是，这次的try catch中调用的是gen.throw(err)
             // 那么自然就被catch到 然后把promise给reject掉了
             function onReject(err) {
-              step('throw', err)
+              step("throw", err);
             }
-          )
+          );
         }
       }
-      step('next')
-    })
-  }
+      step("next");
+    });
+  };
 }
