9de4c606039d45bd558d5769ec3d37f0e23ff270
water.li <289002199@qq.com>
2024-04-20 19:51:55 +0800

    .

--- React/packages/src/react-reconciler/src/ReactFiberFlags.js
+++ React/packages/src/react-reconciler/src/ReactFiberFlags.js
@@ -2,11 +2,11 @@
  * @Author: water.li
  * @Date: 2023-01-03 23:48:27
  * @Description:
- * @FilePath: \Notebook\React\source\src\react-reconciler\src\ReactFiberFlags.js
+ * @FilePath: \Notebook\React\packages\src\react-reconciler\src\ReactFiberFlags.js
  */
 
 export const NoFlags /*           */ = 0b000000000000000000; // 0
 export const Placement /*         */ = 0b000000000000000010; // 2
 export const Update /*            */ = 0b000000000000000100; // 4
 export const ChildDeletion /*     */ = 0b000000000000001000;
-export const MutationMask = Placement | Update;
+export const MutationMask /*      */ = Placement | Update;
