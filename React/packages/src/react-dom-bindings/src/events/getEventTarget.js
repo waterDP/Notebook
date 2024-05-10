d133dd550cce9768411867571a953a3e67c312b5
water.li <289002199@qq.com>
2024-05-10 20:47:49 +0800

    .

--- React/packages/src/react-dom-bindings/src/events/getEventTarget.js
+++ React/packages/src/react-dom-bindings/src/events/getEventTarget.js
@@ -1,4 +1,10 @@
+/*
+ * @Author: water.li
+ * @Date: 2023-04-07 20:32:28
+ * @Description:
+ * @FilePath: \Notebook\React\packages\src\react-dom-bindings\src\events\getEventTarget.js
+ */
 export default function getEventTarget(nativeEvent) {
-  const target = nativeEvent.target || nativeEvent.srcEvent || window;
+  const target = nativeEvent.target || nativeEvent.srcElement || window;
   return target;
 }
