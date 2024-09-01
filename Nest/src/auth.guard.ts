0ddb9d06507c29948650cd6cc75f5d6022756385
water.li <289002199@qq.com>
2024-09-01 16:42:53 +0800

    .

---
+++ Nest/src/auth.guard.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: water.li
+ * @Date: 2024-09-01 16:16:30
+ * @Description:
+ * @FilePath: \Notebook\Nest\src\auth.guard.ts
+ */
+
+import { Injectable, CanActivate } from "@nestjs/common";
+
+@Injectable()
+export class AuthGuard implements CanActivate {}
