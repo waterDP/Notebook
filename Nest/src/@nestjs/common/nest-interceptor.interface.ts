693afbf0834ca04df709e5bd379915a2508037ac
water.li <289002199@qq.com>
2024-09-07 17:31:52 +0800

    .

--- Nest/src/@nestjs/common/nest-interceptor.interface.ts
+++ Nest/src/@nestjs/common/nest-interceptor.interface.ts
@@ -1,5 +1,10 @@
+import { Observable } from "rxjs";
 import { ExecutionContext } from "./execution-context.interface";
 
 export interface NestInterceptor {
   intercept(context: ExecutionContext, next);
+}
+
+export interface CallHandler {
+  handle(): Observable<any>;
 }
--- Nest/src/log1.interceptor.ts
+++ Nest/src/log1.interceptor.ts
@@ -2,18 +2,16 @@ import { NestInterceptor } from "@nestjs/common";
 import { ExecutionContext } from "./@nestjs/common/execution-context.interface";
 import { Observable } from "rxjs";
 import { tap } from "rxjs/operators";
+import { CallHandler } from "./@nestjs/common/nest-interceptor.interface";
 
 export class Logging1Interceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next): Observable<any> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     console.log("before1...");
     const now = Date.now();
-    return next
-      .handle()
-      .pipe(
-        tap(() => {
-          console.log(`after1...${Date.now() - now}ms`);
-        })
-      )
-      .subscribe((value) => value);
+    return next.handle().pipe(
+      tap(() => {
+        console.log(`after1...${Date.now() - now}ms`);
+      })
+    );
   }
 }
