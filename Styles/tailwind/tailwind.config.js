adba0e66c049ab6dde91ca0b409cd001d7f6bb82
water.li <289002199@qq.com>
2024-09-08 17:10:11 +0800

    tailwind

---
+++ Styles/tailwind/tailwind.config.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: water.li
+ * @Date: 2024-09-08 16:48:59
+ * @Description: 
+ * @FilePath: \Notebook\Styles\tailwind\tailwind.config.js
+ */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
