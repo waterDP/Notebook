278254242b0c532bbe1378e71745b0593d1566ab
water.li <289002199@qq.com>
2024-09-21 11:14:28 +0800

    .

--- Nest/src/@nestjs/core/nest-fatory.ts
+++ Nest/src/@nestjs/core/nest-fatory.ts
@@ -1,3 +1,9 @@
+/*
+ * @Author: water.li
+ * @Date: 2024-06-27 21:08:26
+ * @Description: 
+ * @FilePath: \Notebook\Nest\src\@nestjs\core\nest-fatory.ts
+ */
 import { Logger } from "./logger";
 import { NestApplication } from "./nest-application";
 export class NestFactory {
