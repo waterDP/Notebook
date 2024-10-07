7860713e284947d21bdbc3008d89669e432a69f6
water.li <289002199@qq.com>
2024-10-07 12:40:46 +0800

    .

---
+++ Vue/vue记录/文本框大小自适应.vue
@@ -0,0 +1,25 @@
+<script setup>
+const autoHeight = e => {
+  const textarea = e.target
+  textarea.style.height = 'auto'
+  textarea.style.height = textarea.scrollHeight + 'px'
+}
+</script>
+
+<template>
+  <textarea  @input="autoHeight" class="txt" rows="1"></textarea>
+</template>
+
+<style scoped>
+.txt {
+  border: none;
+  outline: none;
+  font-size: 16px;
+  display: block;
+  width: 100%;
+  resize: none;
+  line-height: 2;
+  background: transparent;
+  color: inherit;
+}
+</style>
