c5b90efa8872d39910f591dee0a0944af18336e7
water.li <289002199@qq.com>
2024-10-07 12:28:42 +0800

    .

---
+++ Vue/指令案例/domSizeOb.js
@@ -0,0 +1,25 @@
+const map = new WeakMap()
+
+const ob = new ResizeObserver((entries) => {
+  for (const entry of entries) {
+    const handler = map.get(e)
+    if (handler) {
+      const box = entry.borderBoxSize[0]
+      handler({
+        width: box.inlineSize,
+        height: box.blockSize
+      })
+    }
+  }
+})
+
+export default {
+  mounted(el, binding) {
+    ob.observe(el)
+    map.set(el, binding.value)
+  },
+  unmounted() {
+    // 取消监听
+    ob.unobserve(el)
+  }
+}
