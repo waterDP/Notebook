f287d81dd288f1dff9b69bc5d79be88ab450cd8c
water.li <289002199@qq.com>
2024-11-22 21:31:25 +0800

    1

---
+++ Nest/application/.eslintrc.js
@@ -0,0 +1,32 @@
+/*
+ * @Author: water.li
+ * @Date: 2024-11-22 21:27:39
+ * @Description: 
+ * @FilePath: \Notebook\Nest\application\.eslintrc.js
+ */
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'linebreak-style': ['error', 'auto']
+  },
+};
