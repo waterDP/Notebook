6f740df1b6fbb0f8bd7ee9895771b9827f5d7820
water.li <289002199@qq.com>
2024-11-22 21:34:52 +0800

    11

--- Nest/application/src/app.module.ts
+++ Nest/application/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdminModule } from './admin/admin.module';
+import { SharedModule } from './shared/shared.module';
+import { ApiModule } from './api/api.module';
 
 @Module({
-  imports: [],
+  imports: [AdminModule, SharedModule, ApiModule],
   controllers: [AppController],
   providers: [AppService],
 })
