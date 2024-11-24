93bf85834dc383b02dacff80073b56ec1aedecd9
water.li <289002199@qq.com>
2024-11-24 16:30:25 +0800

    user formater

--- Nest/application/src/shared/entities/user.entities.ts
+++ Nest/application/src/shared/entities/user.entities.ts
@@ -12,22 +12,31 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 export class User {
   @PrimaryGeneratedColumn() // 自增主键列
   id: number;
+
   @Column({ length: 50, unique: true })
   username: string;
+
   @Column()
   password: string;
+
   @Column({ length: 15, nullable: true })
   mobile: string;
+
   @Column({ length: 100, nullable: true })
   email: string;
+
   @Column({ default: 1 }) // 是否生效 1 有效 0 无效
   status: number;
+
   @Column({ default: false }) // 是否为超级管理员
   is_super: boolean;
+
   @Column({ default: 100 }) // 排序编号
   sort: number;
+
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
   createdAt: Date;
+
   @Column({
     type: 'timestamp',
     default: () => 'CURRENT_TIMESTAMP',
