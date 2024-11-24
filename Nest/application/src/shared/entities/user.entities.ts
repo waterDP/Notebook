86f861364fd17e35cc4670630ba1d137832872a3
water.li <289002199@qq.com>
2024-11-24 15:09:15 +0800

    1

--- Nest/application/src/shared/entities/user.entities.ts
+++ Nest/application/src/shared/entities/user.entities.ts
@@ -8,7 +8,7 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 // 实体会映射为数据库的一张表
-@Entity()
+@Entity({ name: 'users' }) // 表名
 export class User {
   @PrimaryGeneratedColumn() // 自增主键列
   id: number;
@@ -20,18 +20,18 @@ export class User {
   mobile: string;
   @Column({ length: 100, nullable: true })
   email: string;
-  @Column({ default: 1 }) // 是否生效 1 有效 0 无效 
+  @Column({ default: 1 }) // 是否生效 1 有效 0 无效
   status: number;
-  @Column({ default: false }) // 是否为超级管理员 
+  @Column({ default: false }) // 是否为超级管理员
   is_super: boolean;
   @Column({ default: 100 }) // 排序编号
   sort: number;
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
-  createAt: Date;
+  createdAt: Date;
   @Column({
     type: 'timestamp',
     default: () => 'CURRENT_TIMESTAMP',
     onUpdate: 'CURRENT_TIMESTAMP',
   })
-  updateAt: Date;
+  updatedAt: Date;
 }
