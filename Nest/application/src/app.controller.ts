ffc71c6557c3a5aa3c36d3c3611124c60ae4f538
water.li <289002199@qq.com>
2024-11-29 23:03:37 +0800

    ./

--- Nest/application/src/app.controller.ts
+++ Nest/application/src/app.controller.ts
@@ -1,3 +1,9 @@
+/*
+ * @Author: water.li
+ * @Date: 2024-11-22 21:27:39
+ * @Description:
+ * @FilePath: \Notebook\Nest\application\src\app.controller.ts
+ */
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
