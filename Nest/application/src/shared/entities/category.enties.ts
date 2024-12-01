c0a1b44641f8413fa338dde969209c1e3dcc7321
water.li <289002199@qq.com>
2024-12-01 19:09:13 +0800

    11

--- Nest/application/src/shared/entities/category.enties.ts
+++ Nest/application/src/shared/entities/category.enties.ts
@@ -7,6 +7,12 @@ import {
   TreeParent,
 } from 'typeorm';
 
+/**
+ * adjacency-list 邻接表
+ * closure-table 闭包表
+ * materialized-path 物化路径
+ * nested-set 嵌套集
+ */
 @Entity()
 @Tree('adjacency-list')
 export class Category {
