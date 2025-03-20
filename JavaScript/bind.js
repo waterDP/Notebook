06c3a3ec00b7d5ba88824d78486f3f2e1566a99f
water.li <289002199@qq.com>
2025-03-20 10:53:17 +0800

    111

--- JavaScript/bind.js
+++ JavaScript/bind.js
@@ -10,14 +10,14 @@
  * 3.如果绑定的函数被new了 当前函数的this就是就是当前的实例
  * 4.new 出来的结果可以找到原有类的结果  
  */
-Function.prototype.bind = function(context) {
+Function.prototype.bind = function (ctx) {
   let that = this
   let bindArgs = Array.prototype.slice.call(arguments, 1)
-  function fBound () {
+  function fBound() {
     let args = Array.prototype.slice.call(arguments, 1)
-    that.apply(this instanceof fBound ? this : context, bindArgs.concat(args))
+    that.apply(this instanceof fBound ? this : ctx, bindArgs.concat(args))
   }
-  function F() {}
+  function F() { }
   F.prototype = this.prototype
   fBound.prototype = new F()
   return fBound
