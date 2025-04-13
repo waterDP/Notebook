e01424e040dcae32245ee5e39a7482810d8d93aa
water.li <289002199@qq.com>
2025-04-13 10:38:02 +0800

    111

--- 前端工程化/rollup/plugin/rollup.config.js
+++
@@ -1,9 +0,0 @@
-import injectPolyfill from "./demo/rollup-plugin-inject-polyfill.js";
-
-export default {
-  input: "./src/index.js",
-  output: {
-    dir: "dist",
-  },
-  plugins: [injectPolyfill()],
-};
---
+++ 前端工程化/rollup/rollup.config.js
@@ -0,0 +1,27 @@
+/*
+ * @Author: water.li
+ * @Date: 2025-04-13 09:42:09
+ * @Description:
+ * @FilePath: \Notebook\前端工程化\Rollup\rollup.config.js
+ */
+import json from "@rollup/plugin-json"
+// npm install --save-dev @rollup/plugin-node-resolve
+import resolve from "@rollup/plugin-node-resolve"
+import commonjs from "@rollup/plugin-commonjs"
+import { terser } from "rollup-plugin-terser"
+export default {
+  input: 'index.js', // 入口文件
+  plugins: [ // 插件
+    json(), // 解析json文件
+    commonjs(), // 将commonjs模块转换为es模块
+    resolve(), // 解析第三方模块
+  ],
+  external: ['react'], // 排除模块
+  output: {
+    file: 'bundle.js', // 输出文件
+    format: 'esm', // 输出模块格式
+    name: 'index', // 导出的模块名
+    plugins: [terser()],
+    banner: `/** this is banner */`
+  }
+}
