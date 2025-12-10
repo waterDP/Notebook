e598a11cdff44226c604628313f7885a592d66b2
water.li <289002199@qq.com>
2025-12-10 08:46:53 +0800

    .call

--- JavaScript/call.js
+++ JavaScript/call.js
@@ -2,20 +2,20 @@
  * @Author: water.li
  * @Date: 2020-03-12 20:33:47
  * @Description: call的实现
- * @FilePath: \notebook\JavaScript\call.js
+ * @FilePath: \Notebook\JavaScript\call.js
  */
 /* 
-	call 的原理
+  call 的原理
 */
-Function.prototype.call = function(context) {
-	context = context ? Object(context) : window
-	context.fn = this
-	let args = []
-	for (let i = 1; i < arguments.length; i++) {
-		args.push('arguments['+i+']')
-	}
-	// 利用数组中的toString的特性
-	let r = eval('context.fn('+args+')')
-	delete context.fn
-	return r
+Function.prototype.call = function (context) {
+  context = context ? Object(context) : window
+  context.fn = this
+  let args = []
+  for (let i = 1; i < arguments.length; i++) {
+    args.push(`arguments[${i}]`)
+  }
+  // 利用数组中的toString的特性
+  let r = eval(`context.fn(${args})`)
+  delete context.fn
+  return r
 }
