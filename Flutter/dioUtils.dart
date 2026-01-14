b84be596b9d53bc48f1f0c3ada10b0f7558cabe0
water.li <289002199@qq.com>
2026-01-14 21:05:03 +0800

    flutter: dio封装

---
+++ Flutter/dioUtils.dart
@@ -0,0 +1,46 @@
+/*
+ * @Author: water.li
+ * @Date: 2026-01-14 20:39:28
+ * @Description: 
+ * @FilePath: \flutter_web_demo\lib\dioUtils.dart
+ */
+
+import 'package:dio/dio.dart';
+
+class DioUtils {
+  final Dio _dio = Dio();
+
+  DioUtils() {
+    _dio.options
+      ..baseUrl = 'https://geek.itheima.net/v1_0'
+      ..connectTimeout = const Duration(seconds: 5)
+      ..connectTimeout = const Duration(seconds: 5)
+      ..receiveTimeout = const Duration(seconds: 5);
+
+    // 拦截器
+    _addInterceptor();
+  }
+
+  void _addInterceptor() {
+    _dio.interceptors.add(InterceptorsWrapper(
+      onRequest: (context, handler) {
+        // handler.next(requestOptions); // 通过
+        // handler.reject(error) // 拦截
+        handler.next(context);
+      },
+      onResponse: (context, handler) {
+        if (context.statusCode! >= 200 && context.statusCode! <= 300) {
+          return handler.next(context);
+        }
+        handler.reject(DioException(requestOptions: context.requestOptions));
+      },
+      onError: (context, handler) {
+        handler.reject(context);
+      },
+    ));
+
+    get(String url, {Map<String, dynamic>? params}) {
+      return _dio.get(url, queryParameters: params);
+    }
+  }
+}
