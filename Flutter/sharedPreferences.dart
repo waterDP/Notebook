2423d41b0314199d56179a565220248fbea37b7b
water.li <289002199@qq.com>
2026-01-15 14:59:23 +0800

    dart: 数据持久化

---
+++ Flutter/sharedPreferences.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Shared Preferences Demo',
+      home: MyHomePage(title: 'Shared Preferences Demo Home Page')
+    )
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadCounter();
+  }
+
+  // 启动时从持久存储中加载初始计数器
+  // 或如果不存在则回退到0
+  Future<void> _loadCounter() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _counter = (prefs.getInt('counter') ?? 0);
+    });
+  }
+
+  // 单击后，递增计数器状态并
+  // 异步将其保存持久存储
+  Future<void> _incrementCounter() async  {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _counter = (prefs.getInt('counter') ?? 0) + 1;
+      prefs.setInt('counter', _counter);
+    })
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title)
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'You have pushed the button this many times:'
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium
+            )
+          ]
+        )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add)
+      )
+    );
+  }
+}
